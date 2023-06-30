f77b91d77e3d7e650426c0a0aab305b9377b0a93
Linwenxuan05 <llwwxxllwwxx@gmail.com>
2023-06-30 14:41:31 +0800

    Implemented Signature Whitelist

--- Lagrange.Core/Utility/Signature.cs
+++ Lagrange.Core/Utility/Signature.cs
@@ -14,13 +14,54 @@ internal static class Signature
     private const string Key = "";
     
     private static bool _available = true;
+    
+    private static readonly string[] WhiteListCommand =
+    {
+        "trpc.o3.ecdh_access.EcdhAccess.SsoEstablishShareKey",
+        "trpc.o3.ecdh_access.EcdhAccess.SsoSecureAccess",
+        "trpc.o3.report.Report.SsoReport",
+        "MessageSvc.PbSendMsg",
+        "wtlogin.trans_emp",
+        "wtlogin.login",
+        "trpc.login.ecdh.EcdhService.SsoKeyExchange",
+        "trpc.login.ecdh.EcdhService.SsoNTLoginPasswordLogin",
+        "trpc.login.ecdh.EcdhService.SsoNTLoginEasyLogin",
+        "trpc.login.ecdh.EcdhService.SsoNTLoginPasswordLoginNewDevice",
+        "trpc.login.ecdh.EcdhService.SsoNTLoginEasyLoginUnusualDevice",
+        "trpc.login.ecdh.EcdhService.SsoNTLoginPasswordLoginUnusualDevice",
+        "OidbSvcTrpcTcp.0x11ec_1",
+        "OidbSvcTrpcTcp.0x758_1",
+        "OidbSvcTrpcTcp.0x7c2_5",
+        "OidbSvcTrpcTcp.0x10db_1",
+        "OidbSvcTrpcTcp.0x8a1_7",
+        "OidbSvcTrpcTcp.0x89a_0",
+        "OidbSvcTrpcTcp.0x89a_15",
+        "OidbSvcTrpcTcp.0x88d_0",
+        "OidbSvcTrpcTcp.0x88d_14",
+        "OidbSvcTrpcTcp.0x112a_1",
+        "OidbSvcTrpcTcp.0x587_74",
+        "OidbSvcTrpcTcp.0x1100_1",
+        "OidbSvcTrpcTcp.0x1102_1",
+        "OidbSvcTrpcTcp.0x1103_1",
+        "OidbSvcTrpcTcp.0x1107_1",
+        "OidbSvcTrpcTcp.0x1105_1",
+        "OidbSvcTrpcTcp.0xf88_1",
+        "OidbSvcTrpcTcp.0xf89_1",
+        "OidbSvcTrpcTcp.0xf57_1",
+        "OidbSvcTrpcTcp.0xf57_106",
+        "OidbSvcTrpcTcp.0xf57_9",
+        "OidbSvcTrpcTcp.0xf55_1",
+        "OidbSvcTrpcTcp.0xf67_1",
+        "OidbSvcTrpcTcp.0xf67_5"
+    };
 
     /// <summary>
     /// Get O3Signature
     /// </summary>
-    public static byte[] GetSignature(string cmd, uint seq, byte[] body)
+    public static byte[]? GetSignature(string cmd, uint seq, byte[] body)
     {
         if (!_available || string.IsNullOrEmpty(Url)) return new byte[20]; // Dummy signature
+        if (!WhiteListCommand.Contains(cmd)) return null;
         
         try
         {
