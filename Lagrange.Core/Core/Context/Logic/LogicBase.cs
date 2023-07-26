32071bcac486117659c6a6a3dbf240795cf0b920
Linwenxuan05 <llwwxxllwwxx@gmail.com>
2023-07-27 05:11:23 +0800

    Updated LogicBase.cs

--- Lagrange.Core/Core/Context/Logic/LogicBase.cs
+++ Lagrange.Core/Core/Context/Logic/LogicBase.cs
@@ -8,5 +8,7 @@ internal abstract class LogicBase
     
     protected LogicBase(ContextCollection collection) => Collection = collection;
 
-    public virtual Task Incoming(ProtocolEvent e) => throw new NotImplementedException();
+    public virtual Task Incoming(ProtocolEvent e) => Task.CompletedTask;
+    
+    public virtual Task Outgoing(ProtocolEvent e) => Task.CompletedTask;
 }
