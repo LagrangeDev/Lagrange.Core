98bd722291c0d2bbbec6bf63bbcfc684082cd5bf
Linwenxuan05 <llwwxxllwwxx@gmail.com>
2023-07-29 17:51:54 +0800

    Update new NTLogin ErrorCode

--- Lagrange.Core/Core/Event/Protocol/Login/LoginCommon.cs
+++ Lagrange.Core/Core/Event/Protocol/Login/LoginCommon.cs
@@ -6,6 +6,7 @@ internal class LoginCommon
     {
         TokenExpired = 140022015,
         UnusualVerify = 140022011,
+        NewDeviceVerify = 14002010,
         CaptchaVerify = 140022008,
         Success = 0,
         Unknown = 1,
