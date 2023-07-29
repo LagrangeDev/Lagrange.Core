333c3b433af1e8e3690f6b81265f409b6aef649c
Linwenxuan05 <llwwxxllwwxx@gmail.com>
2023-07-29 17:57:37 +0800

    Updated LoginCommon.cs

--- Lagrange.Core/Core/Event/Protocol/Login/LoginCommon.cs
+++ Lagrange.Core/Core/Event/Protocol/Login/LoginCommon.cs
@@ -6,7 +6,7 @@ internal class LoginCommon
     {
         TokenExpired = 140022015,
         UnusualVerify = 140022011,
-        NewDeviceVerify = 14002010,
+        NewDeviceVerify = 140022010,
         CaptchaVerify = 140022008,
         Success = 0,
         Unknown = 1,
