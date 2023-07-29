46b5395246139912a9f182f2937df83f8b36aa96
Linwenxuan05 <llwwxxllwwxx@gmail.com>
2023-07-29 19:59:48 +0800

    Downgrade Lagrange.OneBot protocol version

--- Lagrange.OneBot/Constant.cs
+++ Lagrange.OneBot/Constant.cs
@@ -4,7 +4,7 @@ internal static class Constant
 {
     public const string OneBotImpl = "Lagrange.OneBot";
 
-    public const int OneBotProtocolVersion = 12;
+    public const int OneBotProtocolVersion = 11;
     
     public const string OneBotImplVersion = "0.0.1-alpha";
 }
