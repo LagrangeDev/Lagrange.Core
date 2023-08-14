bc7526675c3fd3e67111dcf01f387a854d8b0359
Linwenxuan05 <llwwxxllwwxx@gmail.com>
2023-08-15 02:01:43 +0800

    Corrected Definition for MsgPush

--- Lagrange.Core/Core/Packets/System/NTSysEventSub.cs
+++ Lagrange.Core/Core/Packets/System/NTSysEventSub.cs
@@ -12,11 +12,13 @@ internal class NTSysEventSub
     
     [ProtoMember(3)] public int Field3 { get; set; }
     
-    [ProtoMember(4)] public byte[] Field4 { get; set; }
+    [ProtoMember(4)] public long Field4 { get; set; }
     
     [ProtoMember(5)] public long Uin { get; set; }
     
     [ProtoMember(6)] public int Flag { get; set; }
     
     [ProtoMember(7)] public int On { get; set; }
+    
+    [ProtoMember(8)] public uint GroupUin { get; set; }
 }
