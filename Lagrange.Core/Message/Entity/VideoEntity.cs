89d51f12f14a714e5ed3fc93546ceacb8ffe5db5
Linwenxuan05 <llwwxxllwwxx@gmail.com>
2023-08-20 18:10:02 +0800

    Partially Implemented VideoEntity

---
+++ Lagrange.Core/Message/Entity/VideoEntity.cs
@@ -0,0 +1,44 @@
+using System.Numerics;
+using System.Text;
+using Lagrange.Core.Core.Packets.Message.Element;
+using Lagrange.Core.Core.Packets.Message.Element.Implementation;
+
+namespace Lagrange.Core.Message.Entity;
+
+[MessageElement(typeof(VideoFile))]
+public class VideoEntity : IMessageEntity
+{
+    public string FilePath { get; set; } = string.Empty;
+    
+    public Vector2 Size { get; }
+    
+    public int VideoSize { get; }
+    
+    internal VideoEntity(Vector2 size, int videoSize, string filePath)
+    {
+        Size = size;
+        VideoSize = videoSize;
+        FilePath = filePath;
+    }
+
+    internal VideoEntity() { }
+    
+    IEnumerable<Elem> IMessageEntity.PackElement()
+    {
+        throw new NotImplementedException();
+    }
+
+    IMessageEntity? IMessageEntity.UnpackElement(Elem elem)
+    {
+        if (elem.VideoFile is not { } videoFile) return null;
+        
+        var size = new Vector2(videoFile.FileWidth, videoFile.FileHeight);
+        
+        return new VideoEntity(size, elem.VideoFile.FileSize, Encoding.UTF8.GetString(elem.VideoFile.FileName));
+    }
+
+    public string ToPreviewString()
+    {
+        return $"[Video {Size.X}x{Size.Y}]: {VideoSize}";
+    }
+}
