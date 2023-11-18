590b0e63af9e905e5bdfc846fb4dc358164a0d66
Dreamail <dreaillll@outlook.com>
2023-11-18 22:58:22 +0800

    fix get_group_info & support get_group_member_info and get_group_member_list

--- Lagrange.OneBot/Core/Operation/Info/GetGroupInfoOperation.cs
+++ Lagrange.OneBot/Core/Operation/Info/GetGroupInfoOperation.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using Lagrange.Core;
 using Lagrange.Core.Common.Interface.Api;
+using Lagrange.OneBot.Core.Entity;
 using Lagrange.OneBot.Core.Entity.Action;
 
 namespace Lagrange.OneBot.Core.Operation.Info;
@@ -13,8 +14,14 @@ public class GetGroupInfoOperation : IOperation
     {
         if (payload.Deserialize<OneBotGetGroupInfo>() is { } message)
         {
-            var result = await context.FetchGroups(message.NoCache);
-            return new OneBotResult(result.FirstOrDefault(x => x.GroupUin == message.GroupId), 0, "ok");
+            var result = (await context.FetchGroups(message.NoCache)).FirstOrDefault(x => x.GroupUin == message.GroupId);
+
+            if (result == null)
+            {
+                return new OneBotResult(null, -1, "failed");
+            }
+
+            return new OneBotResult(new OneBotGroup(result.GroupUin, result.GroupName), 0, "ok");
         }
 
         throw new Exception();
