dde03d2f0459562bb493fb023b4618314dcd3222
Linwenxuan <116782992+Linwenxuan05@users.noreply.github.com>
2023-12-21 10:05:39 +0800

    [Core] Add missing Oidbs

---
+++ Lagrange.Core/Internal/Packets/Service/Oidb/Request/OidbSvcTrpcTcp0xFE1_2.cs
@@ -0,0 +1,22 @@
+using ProtoBuf;
+
+namespace Lagrange.Core.Internal.Packets.Service.Oidb.Request;
+
+// Resharper Disable InconsistentNaming
+
+[ProtoContract]
+[OidbSvcTrpcTcp(0xfe1, 2)]
+internal class OidbSvcTrpcTcp0xFE1_2
+{
+    [ProtoMember(1)] public string? Uid { get; set; }
+    
+    [ProtoMember(2)] public uint Field2 { get; set; }
+    
+    [ProtoMember(3)] public List<OidbSvcTrpcTcp0xFE1_2Key>? Keys { get; set; } // can be regarded as constants
+}
+
+[ProtoContract]
+internal class OidbSvcTrpcTcp0xFE1_2Key
+{
+    [ProtoMember(1)] public uint Key { get; set; } // 傻逼
+}
