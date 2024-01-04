3666790853d709d97e5f4db22467bdd5e56c163f
DarkCWK <Dark-Rabbit@outlook.com>
2024-01-04 16:13:18 +0800

    Revert "Fix QrCode display anomaly in terminals that don't support colors"

--- Lagrange.OneBot/Utility/QrCodeHelper.cs
+++ Lagrange.OneBot/Utility/QrCodeHelper.cs
@@ -4,36 +4,27 @@ namespace Lagrange.OneBot.Utility;
 
 internal static class QrCodeHelper
 {
-    // This part of the code is from "https://github.com/eric2788/Lagrange.Core/blob/fd20a5aec81cacd56d60f3130cf057461300fd3f/Lagrange.OneBot/Utility/QrCodeHelper.cs#L30C52-L30C52"
-    // Thanks to "https://github.com/eric2788"
     internal static void Output(string text)
     {
-        var segments = QrSegment.MakeSegments(text);
-        var qrCode = QrCode.EncodeSegments(segments, QrCode.Ecc.Low);
-
+        var qrCode = QrCode.EncodeText(text, QrCode.Ecc.Low);
+        
         for (var y = 0; y < qrCode.Size + 2; y += 2)
         {
             for (var x = 0; x < qrCode.Size + 2; ++x)
             {
-                var foregroundBlack = qrCode.GetModule(x - 1, y - 1);
-                var backgroundBlack = qrCode.GetModule(x - 1, y) || y > qrCode.Size;
+                Console.ForegroundColor = qrCode.GetModule(x - 1, y - 1)
+                    ? ConsoleColor.Black
+                    : ConsoleColor.White;
 
-                if (foregroundBlack && !backgroundBlack)
-                {
-                    Console.Write("▄");
-                } else if (!foregroundBlack && backgroundBlack)
-                {
-                    Console.Write("▀");
-                } else if (foregroundBlack && backgroundBlack)
-                {
-                    Console.Write(" ");
+                Console.BackgroundColor = qrCode.GetModule(x - 1, y) || y > qrCode.Size
+                    ? ConsoleColor.Black
+                    : ConsoleColor.White;
 
-                } else if (!foregroundBlack && !backgroundBlack)
-                {
-                    Console.Write("█");
-                }
+                Console.Write("▀");
             }
             Console.Write("\n");
         }
+        
+        Console.ResetColor();
     }
 }
