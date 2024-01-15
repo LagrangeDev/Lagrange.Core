eae4ac32bb7b4c481b924121017ec5b6ec8d2cd5
NepPure <oyyz@vip.qq.com>
2024-01-15 11:13:21 +0000

    convert by uri

--- Lagrange.OneBot/Core/Message/Entity/ImageSegment.cs
+++ Lagrange.OneBot/Core/Message/Entity/ImageSegment.cs
@@ -30,10 +30,7 @@ public partial class ImageSegment : ISegment
                 
             if (imageSegment.Url.StartsWith("file"))
             {
-                // see: https://github.com/LagrangeDev/Lagrange.Core/issues/69
-                string path = imageSegment.Url.Replace("file:///", "");
-                path = path.Replace("file://", "");
-                path = path.Replace("file:/", "");
+                string path = new Uri(imageSegment.Url).AbsolutePath;
                 builder.Image(File.ReadAllBytes(path));
             }
                 
