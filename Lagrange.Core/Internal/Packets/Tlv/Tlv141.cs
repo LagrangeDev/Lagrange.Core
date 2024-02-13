f5ffc79724009601fe55d61bac8571648817e212
Linwenxuan <116782992+Linwenxuan05@users.noreply.github.com>
2024-02-13 16:11:33 +0800

    [Core] Corrected definition of Tlv141.cs

--- Lagrange.Core/Internal/Packets/Tlv/Tlv141.cs
+++ Lagrange.Core/Internal/Packets/Tlv/Tlv141.cs
@@ -7,7 +7,11 @@ namespace Lagrange.Core.Internal.Packets.Tlv;
 [Tlv(0x141)]
 internal class Tlv141 : TlvBody
 {
-    [BinaryProperty(Prefix.Uint32 | Prefix.LengthOnly)] public string Unknown { get; set; } = "Unknown";
+    [BinaryProperty] public ushort Version { get; set; } = 0;
+    
+    [BinaryProperty(Prefix.Uint16 | Prefix.LengthOnly)] public string Unknown { get; set; } = "Unknown";
 
-    [BinaryProperty] public uint Const0 { get; set; } = 0;
+    [BinaryProperty] public ushort NetworkType { get; set; } = 0;
+    
+    [BinaryProperty(Prefix.Uint16 | Prefix.LengthOnly)] public string Apn { get; set; } = "";
 }
