bf0b93b724e4d645545fbf7dce07955ae066295f
Linwenxuan <116782992+Linwenxuan05@users.noreply.github.com>
2024-02-14 23:16:31 +0800

    [Core] Fixed ZLib validation of LightAppEntity.cs

--- Lagrange.Core/Utility/Binary/Compression/ZCompression.cs
+++ Lagrange.Core/Utility/Binary/Compression/ZCompression.cs
@@ -24,12 +24,13 @@ internal static class ZCompression
     
     public static byte[] ZCompress(string data, byte[]? header = null) => ZCompress(Encoding.UTF8.GetBytes(data), header);
 
-    public static byte[] ZDecompress(byte[] data)
+    public static byte[] ZDecompress(byte[] data, bool validate = true)
     {
         var checksum = data[^4..];
         
         var inflate = Common.Inflate(data[2..^4]);
-        return checksum.SequenceEqual(Adler32(inflate)) ? inflate : throw new Exception("Checksum mismatch");
+        if (validate) return checksum.SequenceEqual(Adler32(inflate)) ? inflate : throw new Exception("Checksum mismatch");
+        return inflate;
     }
         
     private static byte[] Adler32(byte[] data)
