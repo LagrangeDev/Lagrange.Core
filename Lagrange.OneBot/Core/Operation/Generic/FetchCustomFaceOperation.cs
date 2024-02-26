e087ffea385af04355e4ef45a15a83e622cfb509
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-02-26 19:25:37 +0800

    [OneBot] Implemented fetch_custom_face

---
+++ Lagrange.OneBot/Core/Operation/Generic/FetchCustomFaceOperation.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Nodes;
+using Lagrange.Core;
+using Lagrange.Core.Common.Interface.Api;
+using Lagrange.OneBot.Core.Entity.Action;
+
+namespace Lagrange.OneBot.Core.Operation.Generic;
+
+[Operation("fetch_custom_face")]
+internal class FetchCustomFaceOperation : IOperation
+{
+    public async Task<OneBotResult> HandleOperation(BotContext context, JsonNode? payload) => 
+        new(await context.FetchCustomFace(), 200, "ok");
+}
