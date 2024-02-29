4340f254733c91ae923baba0ee6736d9a2149d57
DarkCWK <Dark-Rabbit@outlook.com>
2024-02-29 18:47:32 +0800

    fix base64url length is too long to be parsed

--- Lagrange.OneBot/Message/Entity/CommonResolver.cs
+++ Lagrange.OneBot/Message/Entity/CommonResolver.cs
@@ -6,26 +6,28 @@ public static class CommonResolver
 
     public static byte[]? Resolve(string url)
     {
+        if (url.StartsWith("base64://")) return Convert.FromBase64String(url.Replace("base64://", ""));
+
         Uri uri = new(url);
 
         return uri.Scheme switch
         {
             "http" or "https" => Client.GetAsync(uri).Result.Content.ReadAsByteArrayAsync().Result,
             "file" => File.ReadAllBytes(Path.GetFullPath(uri.AbsolutePath)),
-            "base64" => Convert.FromBase64String(url.Replace("base64://", "")),
             _ => null,
         };
     }
 
     public static Stream? ResolveStream(string url)
     {
+        if (url.StartsWith("base64://")) return new MemoryStream(Convert.FromBase64String(url.Replace("base64://", "")));
+
         Uri uri = new(url);
 
         return uri.Scheme switch
         {
             "http" or "https" => Client.GetAsync(uri).Result.Content.ReadAsStreamAsync().Result,
             "file" => new FileStream(Path.GetFullPath(uri.AbsolutePath), FileMode.Open),
-            "base64" => new MemoryStream(Convert.FromBase64String(url.Replace("base64://", ""))),
             _ => null,
         };
     }
