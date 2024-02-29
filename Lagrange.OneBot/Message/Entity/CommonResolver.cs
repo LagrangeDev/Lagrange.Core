c5212c422b0261ae02ec0a1069cad922462f6997
DarkCWK <Dark-Rabbit@outlook.com>
2024-02-29 18:32:41 +0800

    [OneBot] fix fileurl parsing failure

--- Lagrange.OneBot/Message/Entity/CommonResolver.cs
+++ Lagrange.OneBot/Message/Entity/CommonResolver.cs
@@ -3,48 +3,30 @@ namespace Lagrange.OneBot.Message.Entity;
 public static class CommonResolver
 {
     private static readonly HttpClient Client = new();
-    
+
     public static byte[]? Resolve(string url)
     {
-        if (url.StartsWith("http"))
-        {
-            return Client.GetAsync(url).Result.Content.ReadAsByteArrayAsync().Result;
-        }
-                
-        if (url.StartsWith("file"))
-        {
-            string path = Path.GetFullPath(url.Replace("file://", ""));
-            return File.ReadAllBytes(path);
-        }
-                
-        if (url.StartsWith("base64"))
-        {
-            string base64 = url.Replace("base64://", "");
-            return Convert.FromBase64String(base64);
-        }
+        Uri uri = new(url);
 
-        return null;
+        return uri.Scheme switch
+        {
+            "http" or "https" => Client.GetAsync(uri).Result.Content.ReadAsByteArrayAsync().Result,
+            "file" => File.ReadAllBytes(Path.GetFullPath(uri.AbsolutePath)),
+            "base64" => Convert.FromBase64String(url.Replace("base64://", "")),
+            _ => null,
+        };
     }
 
     public static Stream? ResolveStream(string url)
     {
-        if (url.StartsWith("http"))
-        {
-            return Client.GetAsync(url).Result.Content.ReadAsStreamAsync().Result;
-        }
-                
-        if (url.StartsWith("file"))
-        {
-            string path = Path.GetFullPath(url.Replace("file://", ""));
-            return new FileStream(path, FileMode.Open);
-        }
-                
-        if (url.StartsWith("base64"))
-        {
-            string base64 = url.Replace("base64://", "");
-            return new MemoryStream(Convert.FromBase64String(base64));
-        }
+        Uri uri = new(url);
 
-        return null;
+        return uri.Scheme switch
+        {
+            "http" or "https" => Client.GetAsync(uri).Result.Content.ReadAsStreamAsync().Result,
+            "file" => new FileStream(Path.GetFullPath(uri.AbsolutePath), FileMode.Open),
+            "base64" => new MemoryStream(Convert.FromBase64String(url.Replace("base64://", ""))),
+            _ => null,
+        };
     }
 }
