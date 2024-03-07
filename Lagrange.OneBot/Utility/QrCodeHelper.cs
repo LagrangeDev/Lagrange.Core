e547176e77332eac3d0509b4561f33738ffe0cc6
Xeron <2398003522@qq.com>
2024-03-07 14:23:41 +0800

    [Onebot] Adds compatibility mode to console QrCode.

--- Lagrange.OneBot/Utility/QrCodeHelper.cs
+++ Lagrange.OneBot/Utility/QrCodeHelper.cs
@@ -6,12 +6,14 @@ internal static class QrCodeHelper
 {
     // This part of the code is from "https://github.com/eric2788/Lagrange.Core/blob/fd20a5aec81cacd56d60f3130cf057461300fd3f/Lagrange.OneBot/Utility/QrCodeHelper.cs#L30C52-L30C52"
     // Thanks to "https://github.com/eric2788"
-    internal static void Output(string text)
+    internal static void Output(string text, bool compatibilityMode)
     {
         var segments = QrSegment.MakeSegments(text);
         var qrCode = QrCode.EncodeSegments(segments, QrCode.Ecc.Low);
 
-        for (var y = 0; y < qrCode.Size + 2; y += 2)
+        var (bottomHalfBlock, topHalfBlock, emptyBlock, fullBlock) = compatibilityMode ? (".", "^", " ", "@") : ("▄", "▀", " ", "█");
+
+		for (var y = 0; y < qrCode.Size + 2; y += 2)
         {
             for (var x = 0; x < qrCode.Size + 2; ++x)
             {
@@ -20,20 +22,25 @@ internal static class QrCodeHelper
 
                 if (foregroundBlack && !backgroundBlack)
                 {
-                    Console.Write("▄");
+                    Console.Write(bottomHalfBlock);
                 } else if (!foregroundBlack && backgroundBlack)
                 {
-                    Console.Write("▀");
+                    Console.Write(topHalfBlock);
                 } else if (foregroundBlack && backgroundBlack)
                 {
-                    Console.Write(" ");
+                    Console.Write(emptyBlock);
 
                 } else if (!foregroundBlack && !backgroundBlack)
                 {
-                    Console.Write("█");
+                    Console.Write(fullBlock);
                 }
             }
             Console.Write("\n");
+        }
+
+        if (compatibilityMode)
+        {
+            Console.WriteLine("Please scan this QR code from a distance with your smart phone.\nScanning may fail if you are too close.");
         }
     }
 }
