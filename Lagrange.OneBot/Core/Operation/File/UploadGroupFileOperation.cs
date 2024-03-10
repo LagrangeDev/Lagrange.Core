4056919d38b24eb07b4437daf4edfaaf2cea3c01
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-03-10 10:52:18 +0800

    [OneBot] Implemented get_group_root_files and get_group_files_by_folder

--- Lagrange.OneBot/Core/Operation/File/UploadGroupFileOperation.cs
+++ Lagrange.OneBot/Core/Operation/File/UploadGroupFileOperation.cs
@@ -6,19 +6,19 @@ using Lagrange.Core.Message.Entity;
 using Lagrange.OneBot.Core.Entity.Action;
 using Lagrange.OneBot.Core.Operation.Converters;
 
-namespace Lagrange.OneBot.Core.Operation.Message;
+namespace Lagrange.OneBot.Core.Operation.File;
 
-[Operation("upload_private_file")]
-public class UploadPrivateFile : IOperation
+[Operation("upload_group_file")]
+public class UploadGroupFileOperation : IOperation
 {
     public async Task<OneBotResult> HandleOperation(BotContext context, JsonNode? payload)
     {
-        if (payload.Deserialize<OneBotUploadPrivateFile>(SerializerOptions.DefaultOptions) is { } file)
+        if (payload.Deserialize<OneBotUploadGroupFile>(SerializerOptions.DefaultOptions) is { } file)
         {
             var entity = new FileEntity(file.File);
             if (file.Name != null) entity.FileName = file.Name;
 
-            bool result = await context.UploadFriendFile(file.UserId, entity);
+            bool result = await context.GroupFSUpload(file.GroupId, entity, file.Folder ?? "/");
             return new OneBotResult(null, result ? 0 : 1, result ? "ok" : "failed");
         }
         
--- Lagrange.OneBot/Core/Operation/File/UploadPrivateFileOperation.cs
+++ Lagrange.OneBot/Core/Operation/File/UploadPrivateFileOperation.cs
@@ -6,19 +6,19 @@ using Lagrange.Core.Message.Entity;
 using Lagrange.OneBot.Core.Entity.Action;
 using Lagrange.OneBot.Core.Operation.Converters;
 
-namespace Lagrange.OneBot.Core.Operation.Message;
+namespace Lagrange.OneBot.Core.Operation.File;
 
-[Operation("upload_group_file")]
-public class UploadGroupFileOperation : IOperation
+[Operation("upload_private_file")]
+public class UploadPrivateFile : IOperation
 {
     public async Task<OneBotResult> HandleOperation(BotContext context, JsonNode? payload)
     {
-        if (payload.Deserialize<OneBotUploadGroupFile>(SerializerOptions.DefaultOptions) is { } file)
+        if (payload.Deserialize<OneBotUploadPrivateFile>(SerializerOptions.DefaultOptions) is { } file)
         {
             var entity = new FileEntity(file.File);
             if (file.Name != null) entity.FileName = file.Name;
 
-            bool result = await context.GroupFSUpload(file.GroupId, entity, file.Folder ?? "/");
+            bool result = await context.UploadFriendFile(file.UserId, entity);
             return new OneBotResult(null, result ? 0 : 1, result ? "ok" : "failed");
         }
         
