7de1a7a230f2390854b691f2d491c31419b2477d
ishkong <19740260+ishkong@users.noreply.github.com>
2024-03-17 22:59:14 +0800

    [OneBot] Modify OneBotNode user_id type to be OneBot v11 compliant (#208)
    * [OneBot] Modify OneBotNode user_id type
    
    * [OneBot] Modify `get_forward_msg` user_id type

--- Lagrange.OneBot/Core/Entity/Message/OneBotNode.cs
+++ Lagrange.OneBot/Core/Entity/Message/OneBotNode.cs
@@ -5,9 +5,9 @@ using Lagrange.OneBot.Message;
 namespace Lagrange.OneBot.Core.Entity.Message;
 
 [Serializable]
-public class OneBotNode(uint userId, string nickName, List<OneBotSegment> content) : SegmentBase
+public class OneBotNode(string userId, string nickName, List<OneBotSegment> content) : SegmentBase
 {
-    [JsonPropertyName("user_id")] public uint UserId { get; set; } = userId;
+    [JsonPropertyName("user_id")] public string UserId { get; set; } = userId;
 
     [JsonPropertyName("nickname")] public string NickName { get; set; } = nickName;
 
