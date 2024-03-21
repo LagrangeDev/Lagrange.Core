0622ab868ade4d11ce1cde167998de5692860c96
CrackTC <chenrke369@gmail.com>
2024-03-21 11:53:40 +0800

    [OneBot] HttpPost: add signature support

--- Lagrange.OneBot/Core/Network/Options/HttpPostServiceOptions.cs
+++ Lagrange.OneBot/Core/Network/Options/HttpPostServiceOptions.cs
@@ -5,4 +5,6 @@ public sealed class HttpPostServiceOptions : HttpServiceOptions
     public string Suffix { get; set; } = "";
 
     public uint HeartBeatInterval { get; set; } = 5000;
+
+    public string Secret { get; set; } = string.Empty;
 }
