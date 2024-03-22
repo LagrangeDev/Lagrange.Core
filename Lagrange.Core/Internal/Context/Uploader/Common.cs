1a61f9135bfa937511c0cab9b67f9e642e12f572
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-03-22 19:38:56 +0800

    [Core] minor refactor to VideoUploader.cs

--- Lagrange.Core/Internal/Context/Uploader/Common.cs
+++ Lagrange.Core/Internal/Context/Uploader/Common.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Lagrange.Core.Internal.Event.System;
 using Lagrange.Core.Internal.Packets.Service.Highway;
 using Lagrange.Core.Internal.Packets.Service.Oidb.Common;
@@ -11,6 +12,28 @@ internal static class Common
         var hwUrlEvent = HighwayUrlEvent.Create();
         var highwayUrlResult = await context.Business.SendEvent(hwUrlEvent);
         return ((HighwayUrlEvent)highwayUrlResult[0]).SigSession;
+    }
+
+    public static List<byte[]> CalculateStreamBytes(Stream inputStream)
+    {
+        int currentOffset = 0;
+        const int blockSize = 1024 * 1024;
+        var byteArrayList = new List<byte[]>();
+
+        while (true)
+        {
+            var buffer = new byte[currentOffset + blockSize];
+            inputStream.Seek(0, SeekOrigin.Begin);
+            int length = inputStream.Read(buffer);
+            Array.Resize(ref buffer, length);
+            byteArrayList.Add(SHA1.HashData(buffer));
+            
+            if (length == inputStream.Length) break;
+
+            currentOffset += length;
+        }
+
+        return byteArrayList;
     }
     
     public static NTHighwayNetwork Convert(List<IPv4> ipv4s) => new()
--- Lagrange.Core/Internal/Context/Uploader/VideoUploader.cs
+++ Lagrange.Core/Internal/Context/Uploader/VideoUploader.cs
@@ -11,39 +11,37 @@ internal class VideoUploader : IHighwayUploader
 {
     public async Task UploadPrivate(ContextCollection context, MessageChain chain, IMessageEntity entity)
     {
+        if (entity is VideoEntity { VideoStream: { } stream } video)
         {
-            if (entity is VideoEntity { VideoStream: { } stream } video)
+            var uploadEvent = VideoUploadEvent.Create(video, chain.FriendInfo?.Uid ?? "");
+            var uploadResult = await context.Business.SendEvent(uploadEvent);
+            var metaResult = (VideoUploadEvent)uploadResult[0];
+
+            if (metaResult.UKey != null)
             {
-                var uploadEvent = VideoUploadEvent.Create(video, chain.FriendInfo?.Uid ?? "");
-                var uploadResult = await context.Business.SendEvent(uploadEvent);
-                var metaResult = (VideoUploadEvent)uploadResult[0];
-
-                if (metaResult.UKey != null)
+                var index = metaResult.MsgInfo.MsgInfoBody[0].Index;
+                var extend = new NTV2RichMediaHighwayExt
                 {
-                    var index = metaResult.MsgInfo.MsgInfoBody[0].Index;
-                    var extend = new NTV2RichMediaHighwayExt
-                    {
-                        FileUuid = index.FileUuid,
-                        UKey = metaResult.UKey,
-                        Network = Common.Convert(metaResult.Network),
-                        MsgInfoBody = metaResult.MsgInfo.MsgInfoBody,
-                        BlockSize = 1024 * 1024,
-                        Hash = new NTHighwayHash { FileSha1 = new List<byte[]> { index.Info.FileSha1.UnHex() } }
-                    };
-                    var extStream = extend.Serialize();
+                    FileUuid = index.FileUuid,
+                    UKey = metaResult.UKey,
+                    Network = Common.Convert(metaResult.Network),
+                    MsgInfoBody = metaResult.MsgInfo.MsgInfoBody,
+                    BlockSize = 1024 * 1024,
+                    Hash = new NTHighwayHash { FileSha1 = new List<byte[]> { index.Info.FileSha1.UnHex() } }
+                };
+                var extStream = extend.Serialize();
 
-                    bool hwSuccess = await context.Highway.UploadSrcByStreamAsync(1001, stream, await Common.GetTicket(context), index.Info.FileHash.UnHex(), extStream.ToArray());
-                    if (!hwSuccess)
-                    {
-                        await stream.DisposeAsync();
-                        throw new Exception();
-                    }
+                bool hwSuccess = await context.Highway.UploadSrcByStreamAsync(1001, stream, await Common.GetTicket(context), index.Info.FileHash.UnHex(), extStream.ToArray());
+                if (!hwSuccess)
+                {
+                    await stream.DisposeAsync();
+                    throw new Exception();
                 }
-
-                video.MsgInfo = metaResult.MsgInfo; // directly constructed by Tencent's BDH Server
-                video.Compat = metaResult.Compat; // for legacy QQ
-                await stream.DisposeAsync();
             }
+
+            video.MsgInfo = metaResult.MsgInfo; // directly constructed by Tencent's BDH Server
+            video.Compat = metaResult.Compat; // for legacy QQ
+            await stream.DisposeAsync();
         }
     }
 
@@ -65,7 +63,7 @@ internal class VideoUploader : IHighwayUploader
                     Network = Common.Convert(metaResult.Network),
                     MsgInfoBody = metaResult.MsgInfo.MsgInfoBody,
                     BlockSize = 1024 * 1024,
-                    Hash = new NTHighwayHash { FileSha1 = new List<byte[]> { index.Info.FileSha1.UnHex() } }
+                    Hash = new NTHighwayHash { FileSha1 = Common.CalculateStreamBytes(stream) }
                 };
                 var extStream = extend.Serialize();
 
