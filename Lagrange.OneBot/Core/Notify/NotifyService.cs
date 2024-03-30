54965fea1fbb9d5fbbc57777ef76f2dc798fb450
DarkCWK <Dark-Rabbit@outlook.com>
2024-03-30 23:50:39 +0800

    [One] Fix `sub_typ` incorrect when bot is kicked out (#259)

--- Lagrange.OneBot/Core/Notify/NotifyService.cs
+++ Lagrange.OneBot/Core/Notify/NotifyService.cs
@@ -1,6 +1,7 @@
 using Lagrange.Core;
 using Lagrange.Core.Common.Entity;
 using Lagrange.Core.Common.Interface.Api;
+using Lagrange.Core.Event.EventArg;
 using Lagrange.Core.Message.Entity;
 using Lagrange.OneBot.Core.Entity.Notify;
 using Lagrange.OneBot.Core.Network;
@@ -101,7 +102,13 @@ public sealed class NotifyService(BotContext bot, ILogger<NotifyService> logger,
                 ?.AsParallel()
                 .FirstOrDefault(r => r.EventType == BotGroupRequest.Type.KickMember && r.GroupUin == @event.GroupUin && r.TargetMemberUin == @event.MemberUin);
 
-            string type = @event.Type.ToString().ToLower();
+            string type = @event.Type switch
+            {
+                GroupMemberDecreaseEvent.EventType.KickMe => "kick_me",
+                GroupMemberDecreaseEvent.EventType.Leave => "leave",
+                GroupMemberDecreaseEvent.EventType.Kick => "kick",
+                _ => @event.Type.ToString()
+            };
             await service.SendJsonAsync(new OneBotMemberDecrease(bot.BotUin, type, @event.GroupUin, botGroupRequest?.InvitorMemberUin ?? 0, @event.MemberUin));
         };
 
