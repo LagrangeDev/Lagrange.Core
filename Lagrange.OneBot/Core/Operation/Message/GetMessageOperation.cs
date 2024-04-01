59ebc3b4951e95351da7c474b3c9431d38baedd5
DarkCWK <Dark-Rabbit@outlook.com>
2024-04-02 00:38:34 +0800

    [OneBot] Incorrect `sender` for `get_msg`

--- Lagrange.OneBot/Core/Operation/Message/GetMessageOperation.cs
+++ Lagrange.OneBot/Core/Operation/Message/GetMessageOperation.cs
@@ -21,15 +21,27 @@ public class GetMessageOperation(LiteDatabase database, MessageService service) 
         {
             var record = database.GetCollection<MessageRecord>().FindById(getMsg.MessageId);
             var chain = (MessageChain)record;
-            var sender = chain.GroupMemberInfo == null
-                ? new OneBotSender(chain.GroupMemberInfo?.Uin ?? 0, chain.GroupMemberInfo?.MemberName ?? string.Empty)
-                : new OneBotSender(chain.FriendUin, chain.FriendInfo?.Nickname ?? string.Empty);
+
+            OneBotSender sender;
+            if (chain.GroupMemberInfo != null)
+            {
+                sender = new(chain.GroupMemberInfo.Uin, chain.GroupMemberInfo.MemberName);
+            }
+            else if (chain.FriendInfo != null)
+            {
+                sender = new(chain.FriendInfo.Uin, chain.FriendInfo.Nickname);
+            }
+            else
+            {
+                sender = new(chain.FriendUin, "");
+            }
+
             var elements = service.Convert(chain);
             var response = new OneBotGetMessageResponse(chain.Time, chain.IsGroup ? "group" : "private", record.MessageHash, sender, elements);
-            
+
             return Task.FromResult(new OneBotResult(response, 0, "ok"));
         }
-        
+
         throw new Exception();
     }
 }
