52bb44c6c70bb1b4e3c6445e7545133cda5bd18b
DarkCWK <Dark-Rabbit@outlook.com>
2024-04-02 01:06:51 +0800

    sweet~~

--- Lagrange.OneBot/Core/Operation/Message/GetMessageOperation.cs
+++ Lagrange.OneBot/Core/Operation/Message/GetMessageOperation.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Lagrange.Core;
+using Lagrange.Core.Common.Entity;
 using Lagrange.Core.Message;
 using Lagrange.OneBot.Core.Entity.Action;
 using Lagrange.OneBot.Core.Entity.Action.Response;
@@ -22,19 +23,12 @@ public class GetMessageOperation(LiteDatabase database, MessageService service) 
             var record = database.GetCollection<MessageRecord>().FindById(getMsg.MessageId);
             var chain = (MessageChain)record;
 
-            OneBotSender sender;
-            if (chain.GroupMemberInfo != null)
+            OneBotSender sender = chain switch
             {
-                sender = new(chain.GroupMemberInfo.Uin, chain.GroupMemberInfo.MemberName);
-            }
-            else if (chain.FriendInfo != null)
-            {
-                sender = new(chain.FriendInfo.Uin, chain.FriendInfo.Nickname);
-            }
-            else
-            {
-                sender = new(chain.FriendUin, "");
-            }
+                { GroupMemberInfo: BotGroupMember g } => new(g.Uin, g.MemberName),
+                { FriendInfo: BotFriend f } => new(f.Uin, f.Nickname),
+                _ => new(chain.FriendUin, "")
+            };
 
             var elements = service.Convert(chain);
             var response = new OneBotGetMessageResponse(chain.Time, chain.IsGroup ? "group" : "private", record.MessageHash, sender, elements);
