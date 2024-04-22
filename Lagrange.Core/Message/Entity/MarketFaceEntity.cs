542c00cc329c49ab3b43e915fc0717c9fd58f0b7
DarkCWK <Dark-Rabbit@outlook.com>
2024-04-22 18:28:42 +0800

    Support MarketFace

---
+++ Lagrange.Core/Message/Entity/MarketFaceEntity.cs
@@ -0,0 +1,58 @@
+namespace Lagrange.Core.Message.Entity;
+
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using Lagrange.Core.Internal.Packets.Message.Element;
+using Lagrange.Core.Internal.Packets.Message.Element.Implementation;
+
+[MessageElement(typeof(MarketFace))]
+public class MarketFaceEntity : IMessageEntity
+{
+    public string FaceId { get; } = string.Empty;
+
+    public int TabId { get; }
+
+    public MarketFaceEntity() { }
+
+    public MarketFaceEntity(string faceId, int tabId)
+    {
+        FaceId = faceId;
+        TabId = tabId;
+    }
+
+    IEnumerable<Elem> IMessageEntity.PackElement()
+    {
+        return new Elem[]{
+            new() {
+                MarketFace = new() {
+                    Name = "[\u5546\u57ce\u8868\u60c5]",
+                    ItemType = 6,
+                    Info = 1,
+                    FaceId = Convert.FromHexString(FaceId),
+                    TabId = TabId,
+                    SubType = 3,
+                    Key = "0000000000000000",
+                    // Param = 
+                    // MediaType
+                    Width = 300,
+                    Height = 300,
+                    PbReserve = new() {
+                        // Size = 
+                        Field8 = 1
+                    }
+                }
+            }
+        };
+    }
+
+    IMessageEntity? IMessageEntity.UnpackElement(Elem elem)
+    {
+        if (elem.MarketFace == null) return null;
+
+        return new MarketFaceEntity(Convert.ToHexString(elem.MarketFace.FaceId), elem.MarketFace.TabId);
+    }
+
+    public string ToPreviewString() => $"[{nameof(MarketFaceEntity)}: {TabId} - {FaceId}]";
+}
