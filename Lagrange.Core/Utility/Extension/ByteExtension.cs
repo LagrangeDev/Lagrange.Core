2f729c866300ae90f3b1ea3f3ee34772ef3421cf
dogdie233 <dogdie233@163.com>
2024-04-29 01:09:44 +0800

    [Core] Pref: Faster `Hex` function in `ByteExtension`

--- Lagrange.Core/Utility/Extension/ByteExtension.cs
+++ Lagrange.Core/Utility/Extension/ByteExtension.cs
@@ -1,30 +1,50 @@
-using System.Text;
 using System.Security.Cryptography;
 
 namespace Lagrange.Core.Utility.Extension;
 
 internal static class ByteExtension
 {
+    private static char[] lowerHexMap = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    private static char[] upperHexMap = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
     public static string Hex(this byte[] bytes, bool lower = false, bool space = false)
-    {
-        var sb = new StringBuilder();
-        foreach (byte b in bytes)
-        {
-            sb.Append(b.ToString(lower ? "x2" : "X2"));
-            if (space) sb.Append(' ');
-        }
-        return sb.ToString();
-    }
-    
+        => Hex(bytes.AsSpan(), lower, space);
+
     public static string Hex(this Span<byte> bytes, bool lower = false, bool space = false)
+        => Hex((ReadOnlySpan<byte>)bytes, lower, space);
+
+    public static string Hex(this ReadOnlySpan<byte> bytes, bool lower = false, bool space = false)
+        => space ? HexWithSpace(bytes, lower) : HexNoSpace(bytes, lower);
+
+    public static string HexNoSpace(this ReadOnlySpan<byte> bytes, bool lower = false)
     {
-        var sb = new StringBuilder();
-        foreach (byte b in bytes)
+        Span<char> result = bytes.Length <= 1024 ? stackalloc char[bytes.Length * 2] : new char[bytes.Length * 2];
+        var map = lower ? lowerHexMap : upperHexMap;
+        byte mask = 0x0F;
+
+        for (var i = 0; i < bytes.Length; i++)
         {
-            sb.Append(b.ToString(lower ? "x2" : "X2"));
-            if (space) sb.Append(' ');
+            result[i * 2] = map[(bytes[i] >> 4) & mask];
+            result[i * 2 + 1] = map[bytes[i] & mask];
         }
-        return sb.ToString();
+
+        return new string(result);
+    }
+
+    public static string HexWithSpace(this ReadOnlySpan<byte> bytes, bool lower = false)
+    {
+        Span<char> result = bytes.Length <= 512 ? stackalloc char[bytes.Length * 3] : new char[bytes.Length * 3];
+        byte mask = 0x0F;
+        var map = lower ? lowerHexMap : upperHexMap;
+
+        for (var i = 0; i < bytes.Length; i++)
+        {
+            result[i * 2] = map[(bytes[i] >> 4) & mask];
+            result[i * 2 + 1] = map[bytes[i] & mask];
+            result[i * 2 + 2] = ' ';
+        }
+
+        return new string(result.Slice(0, Math.Max(0, result.Length - 1)));
     }
 
     public static string Md5(this byte[] bytes, bool lower = false)
