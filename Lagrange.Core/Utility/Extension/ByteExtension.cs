9435fb464c8700fc7405dc9db5d468c2186d34c2
dogdie233 <42267840+dogdie233@users.noreply.github.com>
2024-04-29 23:15:29 +0800

    [Core] Perf: Faster `Hex` function in `ByteExtension` (#344)
    * [Core] Pref: Faster `Hex` function in `ByteExtension`
    
    * [Core] chore: merge `HexNoSpace` and `HexWithSpace` function, pref: Use `ArrayPool` as the buffer for large input
    
    * [Core] Pref: improve `Hex` function in `ByteExtension` with Unsafe
    
    * chore: Simplify code
    
    * chore: 移除测试用内容，装傻.jpg
    
    * [Core] fix: Use `MemoryMarshal.CreateSpan` instead of `new Span` to avoid potential GC Hole
    
    * fix: potential leak caused by pointer
    
    * chore: clean workspace

--- Lagrange.Core/Utility/Extension/ByteExtension.cs
+++ Lagrange.Core/Utility/Extension/ByteExtension.cs
@@ -1,30 +1,65 @@
-using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
 namespace Lagrange.Core.Utility.Extension;
 
 internal static class ByteExtension
 {
-    public static string Hex(this byte[] bytes, bool lower = false, bool space = false)
+
+    private interface IHexByteStruct
     {
-        var sb = new StringBuilder();
-        foreach (byte b in bytes)
-        {
-            sb.Append(b.ToString(lower ? "x2" : "X2"));
-            if (space) sb.Append(' ');
-        }
-        return sb.ToString();
+        void Write(uint twoChar);
     }
-    
-    public static string Hex(this Span<byte> bytes, bool lower = false, bool space = false)
+
+    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode, Size = 6)]
+    private struct WithSpaceHexByteStruct : IHexByteStruct
     {
-        var sb = new StringBuilder();
-        foreach (byte b in bytes)
+        [FieldOffset(0)] public uint twoChar;
+        [FieldOffset(4)] public char space;
+
+        public void Write(uint twoChar)
         {
-            sb.Append(b.ToString(lower ? "x2" : "X2"));
-            if (space) sb.Append(' ');
+            this.twoChar = twoChar;
+            space = ' ';
         }
-        return sb.ToString();
+    }
+
+    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode, Size = 4)]
+    private struct NoSpaceHexByteStruct : IHexByteStruct
+    {
+        [FieldOffset(0)] public uint twoChar;
+
+        public void Write(uint twoChar)
+            => this.twoChar = twoChar;
+    }
+
+    public static string Hex(this byte[] bytes, bool lower = false, bool space = false)
+        => Hex(bytes.AsSpan(), lower, space);
+
+    public static string Hex(this Span<byte> bytes, bool lower = true, bool space = false)
+        => Hex((ReadOnlySpan<byte>)bytes, lower, space);
+
+    public static string Hex(this ReadOnlySpan<byte> bytes, bool lower = true, bool space = false)
+        => space ? HexInternal<WithSpaceHexByteStruct>(bytes, lower) : HexInternal<NoSpaceHexByteStruct>(bytes, lower);
+
+    private unsafe static string HexInternal<TStruct>(ReadOnlySpan<byte> bytes, bool lower) where TStruct : struct, IHexByteStruct
+    {
+        if (bytes.Length == 0) return string.Empty;
+
+        var casing = lower ? 0x200020u : 0;
+        var structSize = Marshal.SizeOf<TStruct>();
+        if (structSize % 2 == 1)
+            throw new ArgumentException($"{nameof(TStruct)}'s size of must be a multiple of 2, currently {structSize}");
+
+        var charCountPerByte = structSize / 2;  // 2 is the size of char
+        var result = new string('\0', bytes.Length * charCountPerByte);
+        var resultSpan = MemoryMarshal.CreateSpan(ref Unsafe.As<char, TStruct>(ref Unsafe.AsRef(in result.GetPinnableReference())), bytes.Length);
+
+        for (int i = 0; i < bytes.Length; i++)
+            resultSpan[i].Write(ToCharsBuffer(bytes[i], casing));
+
+        return result;
     }
 
     public static string Md5(this byte[] bytes, bool lower = false)
@@ -55,5 +90,14 @@ internal static class ByteExtension
         using var stream = new MemoryStream(bytes, offset, count);
         var hash = await md5.ComputeHashAsync(stream);
         return hash.Hex(lower);
+    }
+
+    // By Executor-Cheng https://github.com/KonataDev/Lagrange.Core/pull/344#pullrequestreview-2027515322
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint ToCharsBuffer(byte value, uint casing = 0)
+    {
+        uint difference = BitConverter.IsLittleEndian ? ((uint)value >> 4) + ((value & 0x0Fu) << 16) - 0x890089u : ((value & 0xF0u) << 12) + (value & 0x0Fu) - 0x890089u;
+        uint packedResult = ((((uint)(-(int)difference) & 0x700070u) >> 4) + difference + 0xB900B9u) | (uint)casing;
+        return packedResult;
     }
 }
