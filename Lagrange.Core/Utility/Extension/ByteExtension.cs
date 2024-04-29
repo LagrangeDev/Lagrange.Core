628ca5c0466ead60e5413aa75116886643814e49
dogdie233 <dogdie233@163.com>
2024-04-29 21:43:10 +0800

    [Core] Pref: improve `Hex` function in `ByteExtension` with Unsafe

--- Lagrange.Core/Utility/Extension/ByteExtension.cs
+++ Lagrange.Core/Utility/Extension/ByteExtension.cs
@@ -1,50 +1,66 @@
-using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 
 namespace Lagrange.Core.Utility.Extension;
 
 internal static class ByteExtension
 {
-    private static char[] lowerHexMap = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-    private static char[] upperHexMap = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    private interface IHexByteStruct
+    {
+        void Write(uint twoChar);
+    }
+
+    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode, Size = 6)]
+    private struct WithSpaceHexByteStruct : IHexByteStruct
+    {
+        [FieldOffset(0)] public uint twoChar;
+        [FieldOffset(4)] public char space;
+
+        public void Write(uint twoChar)
+        {
+            this.twoChar = twoChar;
+            space = ' ';
+        }
+    }
+
+    [StructLayout(LayoutKind.Explicit, CharSet = CharSet.Unicode, Size = 4)]
+    private struct NoSpaceHexByteStruct : IHexByteStruct
+    {
+        [FieldOffset(0)] public uint twoChar;
+
+        public void Write(uint twoChar)
+        {
+            this.twoChar = twoChar;
+        }
+    }
 
     public static string Hex(this byte[] bytes, bool lower = false, bool space = false)
         => Hex(bytes.AsSpan(), lower, space);
 
-    public static string Hex(this Span<byte> bytes, bool lower = false, bool space = false)
+    public static string Hex(this Span<byte> bytes, bool lower = true, bool space = false)
         => Hex((ReadOnlySpan<byte>)bytes, lower, space);
 
     public static string Hex(this ReadOnlySpan<byte> bytes, bool lower = true, bool space = false)
+        => space ? HexInternal(bytes, lower, new WithSpaceHexByteStruct()) : HexInternal(bytes, lower, new NoSpaceHexByteStruct());
+
+    private unsafe static string HexInternal<TStruct>(ReadOnlySpan<byte> bytes, bool lower, TStruct writer) where TStruct : struct, IHexByteStruct
     {
-        var capacity = (space ? bytes.Length * 3 - 1 : bytes.Length * 2);
-        char[]? rentArray = null;
-        Span<char> buffer = capacity <= 4096 ? stackalloc char[capacity] : (rentArray = ArrayPool<char>.Shared.Rent(capacity)).AsSpan();
-        byte mask = 0x0F;
-        var map = lower ? lowerHexMap : upperHexMap;
+        if (bytes.Length == 0) return string.Empty;
 
-        if (!space)
-        {
-            for (var i = 0; i < bytes.Length; i++)
-            {
-                buffer[i * 2] = map[(bytes[i] >> 4) & mask];
-                buffer[i * 2 + 1] = map[bytes[i] & mask];
-            }
-        }
-        else
-        {
-            for (var i = 0; i < bytes.Length; i++)
-            {
-                buffer[i * 3] = map[(bytes[i] >> 4) & mask];
-                buffer[i * 3 + 1] = map[bytes[i] & mask];
-            }
-            for (var i = 2; i < buffer.Length; i += 3)
-                buffer[i] = ' ';
-        }
+        var casing = lower ? 0x200020u : 0;
+        var structSize = Marshal.SizeOf<TStruct>();
+        if (structSize % 2 == 1)
+            throw new ArgumentException($"{nameof(TStruct)}'s size of must be a multiple of 2, currently {structSize}");
 
-        var str = new string(buffer);
-        if (rentArray != null)
-            ArrayPool<char>.Shared.Return(rentArray);
-        return str;
+        var charCountPerByte = Marshal.SizeOf<TStruct>() / 2;  // 2 is the size of char
+        var result = new string('\0', bytes.Length * charCountPerByte);
+        var resultSpan = new Span<TStruct>(Unsafe.AsPointer(ref Unsafe.AsRef(in result.GetPinnableReference())), bytes.Length);
+
+        for (int i = 0; i < bytes.Length; i++)
+            resultSpan[i].Write(ToCharsBuffer(bytes[i], casing));
+
+        return result;
     }
 
     public static string Md5(this byte[] bytes, bool lower = false)
@@ -75,5 +91,14 @@ internal static class ByteExtension
         using var stream = new MemoryStream(bytes, offset, count);
         var hash = await md5.ComputeHashAsync(stream);
         return hash.Hex(lower);
+    }
+
+    // By Executor-Cheng https://github.com/KonataDev/Lagrange.Core/pull/344#pullrequestreview-2027515322
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint ToCharsBuffer(byte value, uint casing = 0)
+    {
+        uint difference = BitConverter.IsLittleEndian ? ((uint)value >> 4) + ((value & 0x0Fu) << 16) - 0x890089u : ((value & 0xF0u) << 12) + (value & 0x0Fu) - 0x890089u;
+        uint packedResult = ((((uint)(-(int)difference) & 0x700070u) >> 4) + difference + 0xB900B9u) | (uint)casing;
+        return packedResult;
     }
 }
