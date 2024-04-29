c1c4052e0b00e7510b03fb546e4ef3c8a8bce194
dogdie233 <dogdie233@163.com>
2024-04-29 22:52:25 +0800

    chore: 移除测试用内容，装傻.jpg

--- Lagrange.Core.Test/Program.cs
+++ Lagrange.Core.Test/Program.cs
@@ -7,9 +7,6 @@ internal static class Program
 {
     public static async Task Main(string[] args)
     {
-        var data = new byte[] { 0x07, 0x21 };
-        Console.WriteLine(data.Hex(true, true) + ' ');
-
         // BenchmarkRunner.Run<ProtoBufTest>(new DebugBuildConfig());
         await new NTLoginTest().LoginByPassword();
         // await new WtLoginTest().FetchQrCode();
