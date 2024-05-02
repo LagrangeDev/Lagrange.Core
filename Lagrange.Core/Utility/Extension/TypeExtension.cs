59b696074768a481b51dee675cb048847b408696
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-05-03 01:40:12 +0800

    [Core] Minor Refactor towards Utility

--- Lagrange.Core/Utility/Extension/TypeExtension.cs
+++ Lagrange.Core/Utility/Extension/TypeExtension.cs
@@ -4,17 +4,12 @@ namespace Lagrange.Core.Utility.Extension;
 
 internal static class TypeExtension
 {
-    private static readonly Dictionary<Type, object> CachedExpressions = new();
+    private static readonly Dictionary<Type, Func<object>> CachedExpressions = new();
     
     public static object CreateInstance(this Type type, bool cache = true)
     {
         if (!cache) return Activator.CreateInstance(type) ?? throw new InvalidOperationException();
-
-        if (CachedExpressions.TryGetValue(type, out var instance))
-        {
-            var unbox = (Func<object>)instance;
-            return unbox();
-        }
+        if (CachedExpressions.TryGetValue(type, out var instance)) return instance();
 
         var expression = Expression.New(type);
         var lambda = Expression.Lambda<Func<object>>(expression);
