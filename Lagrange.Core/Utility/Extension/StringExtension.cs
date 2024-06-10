020d2671f9790eccd1a695302ba75e939f4fc25e
fairy <me@ilyfairy.com>
2024-06-10 13:15:59 +0800

    [Core] Replace custom logic with .NET BCL methods

--- Lagrange.Core/Utility/Extension/StringExtension.cs
+++ Lagrange.Core/Utility/Extension/StringExtension.cs
@@ -8,17 +8,13 @@ internal static class StringExtension
     {
         if (hex.Length % 2 != 0) throw new ArgumentException("Invalid hex string");
 
-        byte[] bytes = new byte[hex.Length / 2];
-        for (int i = 0; i < hex.Length; i += 2) bytes[i / 2] = byte.Parse(hex.Substring(i, 2), NumberStyles.HexNumber);
-        return bytes;
+        return Convert.FromHexString(hex);
     }
     
     public static byte[] UnHex(this ReadOnlySpan<char> hex)
     {
         if (hex.Length % 2 != 0) throw new ArgumentException("Invalid hex string");
 
-        byte[] bytes = new byte[hex.Length / 2];
-        for (int i = 0; i < hex.Length; i += 2) bytes[i / 2] = byte.Parse(hex.Slice(i, 2), NumberStyles.HexNumber);
-        return bytes;
+        return Convert.FromHexString(hex);
     }
 }
