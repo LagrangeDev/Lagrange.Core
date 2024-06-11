fa0f2158f7065e49efc45b241912c897dc0ac59e
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-06-12 02:39:42 +0800

    [Core] 沟槽的____

--- Lagrange.Core/Internal/Service/System/SetNeedToConfirmSwitchService.cs
+++ Lagrange.Core/Internal/Service/System/SetNeedToConfirmSwitchService.cs
@@ -10,22 +10,26 @@ using ProtoBuf;
 namespace Lagrange.Core.Internal.Service.System;
 
 [EventSubscribe(typeof(SetNeedToConfirmSwitchEvent))]
-[Service("OidbSvcTrpcTcp.0x1270_0")]
+[Service("OidbSvcTrpcTcp.0x1277_0")]
 internal class SetNeedToConfirmSwitchService : BaseService<SetNeedToConfirmSwitchEvent>
 {
     protected override bool Build(SetNeedToConfirmSwitchEvent input, BotKeystore keystore, BotAppInfo appInfo, BotDeviceInfo device,
         out BinaryPacket output, out List<BinaryPacket>? extraPackets)
     {
-        var packet = new OidbSvcTrpcTcpBase<OidbSvcTrpcTcp0x1270_0>(new OidbSvcTrpcTcp0x1270_0
+        var packet = new OidbSvcTrpcTcpBase<OidbSvcTrpcTcp0x1277_0>(new OidbSvcTrpcTcp0x1277_0
         {
-            Device = new OidbSvcTrpcTcp0x1270_0Device
+            Body = new OidbSvcTrpcTcp0x1277_0Body
             {
-                Guid = device.Guid.ToByteArray(),
-                AppId = (uint)appInfo.AppId,
-                PackageName = appInfo.PackageName
-            },
-            EnableNoNeed = input.EnableNoNeed
-        });
+                Device = new OidbSvcTrpcTcp0x1277_0Device
+                {
+                    Guid = device.Guid.ToByteArray(),
+                    AppId = (uint)appInfo.AppId,
+                    PackageName = appInfo.PackageName
+                },
+                GuidEncryptedType = false,
+                AutoLogin = input.EnableNoNeed
+            }
+        }, false, true);
 
         output = packet.Serialize();
         extraPackets = null;
