d5f6bfcc2c3c27c04804d9edb5a56d54f23fe378
DarkCWK <Dark-Rabbit@outlook.com>
2024-07-02 19:49:30 +0800

    [OneBot] fix music property error (#427)

--- Lagrange.OneBot/Message/Entity/MusicSegment.cs
+++ Lagrange.OneBot/Message/Entity/MusicSegment.cs
@@ -7,9 +7,9 @@ using Lagrange.OneBot.Utility;
 namespace Lagrange.OneBot.Message.Entity;
 
 [Serializable]
-public partial class MusicSegment(string type, string url, string audio, string title, string image, string singer)
+public partial class MusicSegment(string type, string url, string audio, string title, string image, string content)
 {
-    public MusicSegment() : this("","","","","","") { }
+    public MusicSegment() : this("", "", "", "", "", "") { }
 
     [JsonPropertyName("type")][CQProperty] public string Type { get; set; } = type;
 
@@ -19,9 +19,9 @@ public partial class MusicSegment(string type, string url, string audio, string 
 
     [JsonPropertyName("title")][CQProperty] public string Title { get; set; } = title;
 
+    [JsonPropertyName("content")][CQProperty] public string Content { get; set; } = content;
+    
     [JsonPropertyName("image")][CQProperty] public string Image { get; set; } = image;
-
-    [JsonPropertyName("singer")][CQProperty] public string Singer { get; set; } = singer;
 }
 
 [SegmentSubscriber(typeof(ImageEntity), "music")]
