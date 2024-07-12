fda7437fc232b2f262ec39cf0f443e920706ecf5
DarkCWK <Dark-Rabbit@outlook.com>
2024-07-13 00:09:16 +0800

    （；´д｀）ゞ

--- Lagrange.OneBot/Utility/AudioHelper.cs
+++ Lagrange.OneBot/Utility/AudioHelper.cs
@@ -30,72 +30,78 @@ public static class AudioHelper
         return (type = DetectAudio(data)) != AudioFormat.Unknown;
     }
 
-    private static readonly KeyValuePair<UInt128, AudioFormat>[] _mapper =
-    [
-        // WAV
-        //  R  I  F  F              W  A  V  E  f  m  t
-        // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        // |52|49|46|46|  |  |  |  |57|41|56|45|66|6D|74|
-        // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        // 0           4           8           12       15
-        new(new(0x5249464600000000UL, 0x57415645666D7400UL), AudioFormat.Wav),
-        // TENSILKV3
-        //     #  !  S  I  L  K
-        // +--+--+--+--+--+--+--+
-        // |02|23|21|53|49|4C|4B|
-        // +--+--+--+--+--+--+--+
-        // 0           4        7
-        new(new(0x02232153494C4B00UL, 0x0000000000000000UL), AudioFormat.TenSilkV3),
-        // AMR
-        //  #  !  A  M  R
-        // +--+--+--+--+--+
-        // |23|21|41|4D|52|
-        // +--+--+--+--+--+
-        // 0           4  5
-        new(new(0x2321414D52000000UL, 0x0000000000000000UL), AudioFormat.Amr),
-        // SILKV3
-        //  #  !  S  I  L  K
-        // +--+--+--+--+--+--+
-        // |23|21|53|49|4C|4B|
-        // +--+--+--+--+--+--+
-        // 0           4     6
-        new(new(0x232153494C4B0000UL, 0x0000000000000000UL), AudioFormat.SilkV3),
-        // Ogg
-        //  O  g  g  S
-        // +--+--+--+--+
-        // |4F|67|67|53|
-        // +--+--+--+--+
-        // 0           4
-        new(new(0x4F67675300000000UL, 0x0000000000000000UL), AudioFormat.Ogg),
-        // MP3 ID3
-        //  I  D  3
-        // +--+--+--+
-        // |49|44|33|
-        // +--+--+--+
-        // 0        3
-        new(new(0x4944330000000000UL, 0x0000000000000000UL), AudioFormat.Mp3),
-        // MP3 no ID3
-        //  ÿ  û
-        // +--+--+
-        // |FF|F2|
-        // +--+--+
-        // 0     2
-        new(new(0xFFF2000000000000UL,0x0000000000000000UL), AudioFormat.Mp3),
-        // MP3 no ID3
-        //  ÿ  û
-        // +--+--+
-        // |FF|F3|
-        // +--+--+
-        // 0     2
-        new(new(0xFFF3000000000000UL,0x0000000000000000UL), AudioFormat.Mp3),
-        // MP3 no ID3
-        //  ÿ  û
-        // +--+--+
-        // |FF|FB|
-        // +--+--+
-        // 0     2
-        new(new(0xFFFB000000000000UL,0x0000000000000000UL), AudioFormat.Mp3),
-    ];
+    // WAV
+    //  R  I  F  F              W  A  V  E  f  m  t
+    // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    // |52|49|46|46|  |  |  |  |57|41|56|45|66|6D|74|
+    // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+    // 0           4           8           12       15
+    private const ulong WAV_HEAD_LOWER = 0x5249464600000000UL;
+    private const ulong WAV_HEAD_UPPER = 0x57415645666D7400UL;
+
+    // TENSILKV3
+    //     #  !  S  I  L  K
+    // +--+--+--+--+--+--+--+
+    // |02|23|21|53|49|4C|4B|
+    // +--+--+--+--+--+--+--+
+    // 0           4        7
+    private const ulong TENSILKV3_HEAD_LOWER = 0x02232153494C4B00UL;
+
+    // AMR
+    //  #  !  A  M  R
+    // +--+--+--+--+--+
+    // |23|21|41|4D|52|
+    // +--+--+--+--+--+
+    // 0           4  5
+    private const ulong AMR_HEAD_LOWER = 0x2321414D52000000UL;
+
+    // SILKV3
+    //  #  !  S  I  L  K
+    // +--+--+--+--+--+--+
+    // |23|21|53|49|4C|4B|
+    // +--+--+--+--+--+--+
+    // 0           4     6
+    private const ulong SILKV3_HEAD_LOWER = 0x232153494C4B0000UL;
+
+    // Ogg
+    //  O  g  g  S
+    // +--+--+--+--+
+    // |4F|67|67|53|
+    // +--+--+--+--+
+    // 0           4
+    private const ulong OGG_HEAD_LOWER = 0x4F67675300000000UL;
+
+    // MP3 ID3
+    //  I  D  3
+    // +--+--+--+
+    // |49|44|33|
+    // +--+--+--+
+    // 0        3
+    private const ulong MP3ID3_HEAD_LOWER = 0x4944330000000000UL;
+
+    // MP3 no ID3
+    //  ÿ  û
+    // +--+--+
+    // |FF|F2|
+    // +--+--+
+    // 0     2
+    private const ulong MP3FFF2_HEAD_LOWER = 0xFFF2000000000000UL;
+
+    // MP3 no ID3
+    //  ÿ  û
+    // +--+--+
+    // |FF|F3|
+    // +--+--+
+    // 0     2
+    private const ulong MP3FFF3_HEAD_LOWER = 0xFFF3000000000000UL;
+
+    // MP3 no ID3
+    //  ÿ  û
+    // +--+--+
+    // |FF|FB|
+    // +--+--+
+    // 0     2
+    private const ulong MP3FFFB_HEAD_LOWER = 0xFFFB000000000000UL;
 
     /// <summary>
     /// Detect audio type
@@ -105,28 +111,29 @@ public static class AudioHelper
     /// <returns></returns>
     public static AudioFormat DetectAudio(byte[] data)
     {
-#if NET8_0_OR_GREATER
-        UInt128 value = BinaryPrimitives.ReadUInt128BigEndian(data.AsSpan(0, 16));
-#else
-        UInt128 value;
-        if (BitConverter.IsLittleEndian)
-        {
-            // UInt128 internally handles low and high bits according to BigEndian and LittleEndian?
-            value = new(
-                BinaryPrimitives.ReverseEndianness(MemoryMarshal.Read<ulong>(data.AsSpan(0, 8))),
-                BinaryPrimitives.ReverseEndianness(MemoryMarshal.Read<ulong>(data.AsSpan(8, 8)))
-            );
-        }
-        else
-        {
-            value = MemoryMarshal.Read<UInt128>(data.AsSpan(0, 16));
-        }
-#endif
+        ulong lower = BinaryPrimitives.ReadUInt64BigEndian(data.AsSpan(0, sizeof(ulong)));
+        ulong upper = BinaryPrimitives.ReadUInt64BigEndian(data.AsSpan(8, sizeof(ulong)));
 
-        foreach ((UInt128 head, AudioFormat format) in _mapper)
-        {
-            if ((value & head) == head) return format;
-        }
+        if ((lower & WAV_HEAD_LOWER) == WAV_HEAD_LOWER &&
+            (upper & WAV_HEAD_UPPER) == WAV_HEAD_UPPER
+        ) return AudioFormat.Wav;
+
+        if ((lower & TENSILKV3_HEAD_LOWER) == TENSILKV3_HEAD_LOWER) return AudioFormat.TenSilkV3;
+
+        if ((lower & AMR_HEAD_LOWER) == AMR_HEAD_LOWER) return AudioFormat.Amr;
+
+        if ((lower & SILKV3_HEAD_LOWER) == SILKV3_HEAD_LOWER) return AudioFormat.SilkV3;
+
+        if ((lower & OGG_HEAD_LOWER) == OGG_HEAD_LOWER) return AudioFormat.Ogg;
+
+        if ((lower & MP3ID3_HEAD_LOWER) == MP3ID3_HEAD_LOWER) return AudioFormat.Mp3;
+
+        if ((lower & MP3FFF2_HEAD_LOWER) == MP3FFF2_HEAD_LOWER) return AudioFormat.Mp3;
+
+        if ((lower & MP3FFF3_HEAD_LOWER) == MP3FFF3_HEAD_LOWER) return AudioFormat.Mp3;
+
+        if ((lower & MP3FFFB_HEAD_LOWER) == MP3FFFB_HEAD_LOWER) return AudioFormat.Mp3;
+
         return AudioFormat.Unknown;
     }
 
