bf77dbc5e1fb130f68739b4286c878f2935f8ccb
DarkCWK <Dark-Rabbit@outlook.com>
2024-07-12 17:38:41 +0800

    ヾ(•ω•`)o

--- Lagrange.OneBot/Utility/AudioHelper.cs
+++ Lagrange.OneBot/Utility/AudioHelper.cs
@@ -25,23 +25,31 @@ public static class AudioHelper
     /// <returns></returns>
     public static AudioFormat DetectAudio(byte[] data)
     {
-        uint value = BinaryPrimitives.ReadUInt32BigEndian(data.AsSpan(0, sizeof(uint)));
+        // special
 
-        // SILKV3
-        //  #  !  S  I  L  K
-        // +--+--+--+--+--+--+
-        // |23|21|53|49|4C|4B|
-        // +--+--+--+--+--+--+
-        // 0           4     6  
-        if (value == 0x23215349) return AudioFormat.SilkV3;
+        // WAV
+        //  R  I  F  F              W  A  V  E  f  m  t
+        // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        // |52|49|46|46|  |  |  |  |57|41|56|45|66|6D|74|
+        // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        // 0           4           8           12       15
+        if (
+            BinaryPrimitives.ReadUInt32BigEndian(data.AsSpan(0, sizeof(uint))) == 0x52494646U &&
+            BinaryPrimitives.ReadUInt64BigEndian(data.AsSpan(7, sizeof(ulong))) << 8 == 0x57415645666D7400UL
+        )
+        {
+            return AudioFormat.Wav;
+        }
+
+        ulong value = BinaryPrimitives.ReadUInt64BigEndian(data.AsSpan(0, sizeof(ulong)));
 
         // TENSILKV3
         //     #  !  S  I  L  K
         // +--+--+--+--+--+--+--+
         // |02|23|21|53|49|4C|4B|
         // +--+--+--+--+--+--+--+
-        // 0           4        7  
-        if (value == 0x02232153) return AudioFormat.TenSilkV3;
+        // 0           4        7
+        if (value >> 8 == 0x0002232153494C4BUL) return AudioFormat.TenSilkV3;
 
         // AMR
         //     #  !  A  M  R
@@ -49,7 +57,22 @@ public static class AudioHelper
         // |02|23|21|41|4D|52|
         // +--+--+--+--+--+--+
         // 0           4     6
-        if (value == 0x2321414D) return AudioFormat.Amr;
+        if (value >> 16 == 0x0000022321414D52UL) return AudioFormat.Amr;
+
+        // SILKV3
+        //  #  !  S  I  L  K
+        // +--+--+--+--+--+--+
+        // |23|21|53|49|4C|4B|
+        // +--+--+--+--+--+--+
+        // 0           4     6
+        if (value >> 16 == 0x0000232153494C4BUL) return AudioFormat.SilkV3;
+
+        // Ogg
+        //  O  g  g  S
+        // +--+--+--+--+
+        // |4F|67|67|53
+        // +--+--+--+--+
+        if (value >> 32 == 0x000000004F676753UL) return AudioFormat.Ogg;
 
         // MP3 ID3
         //  I  D  3
@@ -57,47 +80,31 @@ public static class AudioHelper
         // |49|44|33|
         // +--+--+--+
         // 0        3
-        if (value >> 8 == 0x494433) return AudioFormat.Mp3;
+        if (value >> 40 == 0x0000000000494433UL) return AudioFormat.Mp3;
 
+        // MP3 no ID3
+        //  ÿ  û
+        // +--+--+
+        // |FF|F2|
+        // +--+--+
+        // 0     2
+        if (value >> 48 == 0x000000000000FFF2UL) return AudioFormat.Mp3;
 
-        // Ogg
-        //  O  g  g  S
-        // +--+--+--+--+
-        // |4F|67|67|53
-        // +--+--+--+--+
-        if (value == 0x4F676753) return AudioFormat.Ogg;
+        // MP3 no ID3
+        //  ÿ  û
+        // +--+--+
+        // |FF|F3|
+        // +--+--+
+        // 0     2
+        if (value >> 48 == 0x000000000000FFF3UL) return AudioFormat.Mp3;
 
-        uint mp3NoId3Value = value >> 16;
         // MP3 no ID3
         //  ÿ  û
         // +--+--+
         // |FF|FB|
         // +--+--+
         // 0     2
-        if (mp3NoId3Value == 0xFFFB) return AudioFormat.Mp3;
-        //  ÿ  ó
-        // +--+--+
-        // |FF|F3|
-        // +--+--+
-        // 0     2
-        if (mp3NoId3Value == 0xFFF3) return AudioFormat.Mp3;
-        //  ÿ  ò
-        // +--+--+
-        // |FF|F2|
-        // +--+--+
-        // 0     2
-        if (mp3NoId3Value == 0xFFF2) return AudioFormat.Mp3;
-
-        // WAV
-        //  R  I  F  F  .  .  .  .  W  A  V  E  f  m  t
-        // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        // |52|49|46|46|00|00|00|00|57|41|56|45|66|6D|74|
-        // +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
-        // 0           4           8           12       15
-        if (value == 0x52494646 && BinaryPrimitives.ReadUInt32BigEndian(data.AsSpan(8, sizeof(uint))) == 0x57415645)
-        {
-            return AudioFormat.Wav;
-        }
+        if (value >> 48 == 0x000000000000FFFBUL) return AudioFormat.Mp3;
 
         return AudioFormat.Unknown;
     }
