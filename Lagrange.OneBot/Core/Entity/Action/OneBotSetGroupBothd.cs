fb8873c5174c89c983a6f14e6043e77086c88583
Decrabbityyy <99632363+Decrabbityyy@users.noreply.github.com>
2024-08-15 10:56:40 +0800

    [All] Add data(key) to SGBh (#497)
    * [All] Add data(key) to SGBh
    
    * hardly sleep
    
    * fix the issues

--- Lagrange.OneBot/Core/Entity/Action/OneBotSetGroupBothd.cs
+++ Lagrange.OneBot/Core/Entity/Action/OneBotSetGroupBothd.cs
@@ -8,5 +8,9 @@ public class OneBotSetGroupBothd
     [JsonPropertyName("group_id")] public uint GroupId { get; set; }
     
     [JsonPropertyName("bot_id")] public uint BotId { get; set; }
+
+    [JsonPropertyName("data_1")] public string? Data_1 { get; set; }
+
+    [JsonPropertyName("data_2")] public string? Data_2 { get; set; }
     
 }
