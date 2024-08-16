6b5f52d61b85f547d24caae666c61698358735e7
CwkDark <177549718+CwkDark@users.noreply.github.com>
2024-08-16 15:17:48 +0800

    remove todo

--- Lagrange.Core/Message/MessageChain.cs
+++ Lagrange.Core/Message/MessageChain.cs
@@ -81,7 +81,6 @@ public sealed class MessageChain : List<IMessageEntity>
         var chainBuilder = new StringBuilder();
 
         chainBuilder.Append("[MessageChain");
-        chainBuilder.Append($" {MessageId} "); // TODO Dark/Delete
         if (GroupUin != null) chainBuilder.Append($"({GroupUin})");
         chainBuilder.Append($"({FriendUin})");
         chainBuilder.Append("] ");
