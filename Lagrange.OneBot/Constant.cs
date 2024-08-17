a387edec8fbdad46a55620cc7ca3d342b9c8bff3
CwkDark <177549718+CwkDark@users.noreply.github.com>
2024-08-17 21:54:47 +0800

    [OneBot] format (#515)

--- Lagrange.OneBot/Constant.cs
+++ Lagrange.OneBot/Constant.cs
@@ -5,10 +5,10 @@ internal static class Constant
     public const string OneBotImpl = "Lagrange.OneBot";
 
     public const int OneBotProtocolVersion = 11;
-    
+
     public const string OneBotImplVersion = "0.0.3";
 
     public const bool CanSendImage = true;
-    
+
     public const bool CanSendRecord = true;
 }
