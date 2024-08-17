92883295742875bb4e2961e3408976ee29ea126c
Linwenxuan04 <llwwxxllwwxx@gmail.com>
2024-08-18 05:05:04 +0800

    [OneBot] Fix Format and Incompatibility with System Proxy of OneBotSigner.cs

--- Lagrange.OneBot/Utility/OneBotSigner.cs
+++ Lagrange.OneBot/Utility/OneBotSigner.cs
@@ -1,8 +1,10 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using Lagrange.Core.Utility.Sign;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -12,16 +14,13 @@ namespace Lagrange.OneBot.Utility;
 public class OneBotSigner : SignProvider
 {
     private readonly string? _signServer;
-    private readonly ILogger _logger;
 
     private readonly HttpClient _client;
 
     public OneBotSigner(IConfiguration config, ILogger<OneBotSigner> logger)
     {
         _signServer = config["SignServerUrl"] ?? "";
-        _logger = logger;
-        var signProxyUrl = config["SignProxyUrl"];
-        //Only support HTTP proxy
+        string? signProxyUrl = config["SignProxyUrl"]; // Only support HTTP proxy
 
         _client = new HttpClient(handler: new HttpClientHandler
         {
@@ -31,51 +30,44 @@ public class OneBotSigner : SignProvider
                 BypassProxyOnLocal = false,
                 UseDefaultCredentials = false,
             },
-            UseProxy = !string.IsNullOrEmpty(signProxyUrl),
-        },
-        disposeHandler: true);
+        }, disposeHandler: true);
 
         if (string.IsNullOrEmpty(_signServer)) logger.LogWarning("Signature Service is not available, login may be failed");
     }
 
     public override byte[]? Sign(string cmd, uint seq, byte[] body, [UnscopedRef] out byte[]? e, [UnscopedRef] out string? t)
     {
-        // result
         e = null;
         t = null;
 
         if (!WhiteListCommand.Contains(cmd)) return null;
-
         if (_signServer == null) throw new Exception("Sign server is not configured");
 
-        HttpRequestMessage request = new()
+        using var request = new HttpRequestMessage
         {
             Method = HttpMethod.Post,
-            RequestUri = new(_signServer),
-            Content = new StringContent(
-                $"{{\"cmd\":\"{cmd}\",\"seq\":{seq},\"src\":\"{Convert.ToHexString(body)}\"}}",
-                new MediaTypeHeaderValue("application/json")
-            )
+            RequestUri = new Uri(_signServer),
+            Content = JsonContent.Create(new JsonObject
+            {
+                { "cmd", cmd },
+                { "seq", seq },
+                { "body", Convert.ToHexString(body) }
+            })
         };
 
-        HttpResponseMessage message = _client.Send(request);
-
+        using var message = _client.Send(request);
         if (message.StatusCode != HttpStatusCode.OK) throw new Exception($"Signer server returned a {message.StatusCode}");
+        var json = JsonDocument.Parse(message.Content.ReadAsStream()).RootElement;
 
-        JsonElement json = JsonDocument.Parse(message.Content.ReadAsStream()).RootElement;
-
-        JsonElement valueJson = json.GetProperty("value");
-
-        JsonElement extraJson = valueJson.GetProperty("extra");
-        JsonElement tokenJson = valueJson.GetProperty("token");
-        JsonElement signJson = valueJson.GetProperty("sign");
-
-        string? extra = extraJson.GetString();
-        e = extra != null ? Convert.FromHexString(extra) : [];
+        var valueJson = json.GetProperty("value");
+        var extraJson = valueJson.GetProperty("extra");
+        var tokenJson = valueJson.GetProperty("token");
+        var signJson = valueJson.GetProperty("sign");
 
         string? token = tokenJson.GetString();
+        string? extra = extraJson.GetString();
+        e = extra != null ? Convert.FromHexString(extra) : [];
         t = token != null ? Encoding.UTF8.GetString(Convert.FromHexString(token)) : "";
-
         string sign = signJson.GetString() ?? throw new Exception("Signer server returned an empty sign");
         return Convert.FromHexString(sign);
     }
