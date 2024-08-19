28752e26b611e99d8f0c85e6b2cdaaf1e784b789
DarkCWK <177549718+CwkDark@users.noreply.github.com>
2024-08-20 03:02:15 +0800

    [All] Poke add strength

--- Lagrange.Core/Internal/Packets/Message/Component/Extra/PokeExtra.cs
+++ Lagrange.Core/Internal/Packets/Message/Component/Extra/PokeExtra.cs
@@ -7,7 +7,7 @@ internal class PokeExtra
 {
     [ProtoMember(1)] public uint Type { get; set; }
     
-    [ProtoMember(7)] public uint Field7 { get; set; }
+    [ProtoMember(7)] public uint Strength { get; set; }
     
     [ProtoMember(8)] public uint Field8 { get; set; }
 }
