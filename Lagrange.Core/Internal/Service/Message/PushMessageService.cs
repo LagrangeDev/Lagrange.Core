0ed59da2021417ae5f2ce167fb7420208cf049e2
ishkong <19740260+ishkong@users.noreply.github.com>
2024-08-19 22:49:15 +0800

    [Core] Fix GroupGreyTipNotice may not have 'action_str'

--- Lagrange.Core/Internal/Service/Message/PushMessageService.cs
+++ Lagrange.Core/Internal/Service/Message/PushMessageService.cs
@@ -198,7 +198,15 @@ internal class PushMessageService : BaseService<PushMessageEvent>
 
                 if (greyTip.GeneralGrayTip.BusiType == 12)  // poke
                 {
-                    var groupPokeEvent = GroupSysPokeEvent.Result(groupUin, uint.Parse(templates["uin_str1"]), uint.Parse(templates["uin_str2"]), templates["action_str"], templates["suffix_str"]);
+                    string actionStr;
+                    if (!templates.TryGetValue("action_str", out actionStr))
+                    {
+                        if (!templates.TryGetValue("action_img_url", out actionStr))
+                        {
+                            actionStr = string.Empty;
+                        }
+                    }
+                    var groupPokeEvent = GroupSysPokeEvent.Result(groupUin, uint.Parse(templates["uin_str1"]), uint.Parse(templates["uin_str2"]), actionStr, templates["suffix_str"]);
                     extraEvents.Add(groupPokeEvent);
                 }
                 break;
