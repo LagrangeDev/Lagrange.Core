27d2bdd52a731566e1629a2f0654fcc26e5f2829
CwkDark <177549718+CwkDark@users.noreply.github.com>
2024-08-20 16:39:01 +0800

    [All] Poke add strength (#526)

--- Lagrange.OneBot/Message/Entity/PokeSegment.cs
+++ Lagrange.OneBot/Message/Entity/PokeSegment.cs
@@ -5,12 +5,14 @@ using Lagrange.Core.Message.Entity;
 namespace Lagrange.OneBot.Message.Entity;
 
 [Serializable]
-public partial class PokeSegment(uint type)
+public partial class PokeSegment(uint type, uint strength)
 {
-    public PokeSegment() : this(0) { }
+    public PokeSegment() : this(0, 0) { }
 
     [JsonPropertyName("type")] [CQProperty] public string Type { get; set; } = type.ToString();
     
+    [JsonPropertyName("strength")] public string? Strength { get; set; } = strength.ToString();
+
     [JsonPropertyName("id")] [CQProperty] public string Id { get; set; } = string.Empty;
 }
 
@@ -19,13 +21,13 @@ public partial class PokeSegment : SegmentBase
 {
     public override void Build(MessageBuilder builder, SegmentBase segment)
     {
-        if (segment is PokeSegment pokeSegment) builder.Poke(uint.Parse(pokeSegment.Type));
+        if (segment is PokeSegment pokeSegment) builder.Poke(uint.Parse(pokeSegment.Type), pokeSegment.Strength == null ? 0 : uint.Parse(pokeSegment.Strength));
     }
 
     public override SegmentBase FromEntity(MessageChain chain, IMessageEntity entity)
     {
         if (entity is not PokeEntity pokeEntity) throw new ArgumentException("Invalid entity type.");
         
-        return new PokeSegment(pokeEntity.Type);
+        return new PokeSegment(pokeEntity.Type, pokeEntity.Strength);
     }
 }
