e1bdddbe53b9da7c66e18b2e568cbaf2485cd1f5
ishkong <19740260+ishkong@users.noreply.github.com>
2024-08-20 17:15:28 +0800

    Update OneBotFriendPoke.cs

--- Lagrange.OneBot/Core/Entity/Notify/OneBotFriendPoke.cs
+++ Lagrange.OneBot/Core/Entity/Notify/OneBotFriendPoke.cs
@@ -11,9 +11,11 @@ public class OneBotFriendPoke(uint selfId) : OneBotNotify(selfId, "notify")
 
     [JsonPropertyName("user_id")] public uint UserId { get; set; }
 
-    [JsonPropertyName("target_id")] public uint TargetId { get; set; } // self
+    [JsonPropertyName("target_id")] public uint TargetId { get; set; }
 
     [JsonPropertyName("action")] public string Action { get; set; } = string.Empty;
 
     [JsonPropertyName("suffix")] public string Suffix { get; set; } = string.Empty;
+  
+    [JsonPropertyName("action_img_url")] public string ActionImgUrl { get; set; } = string.Empty;
 }
