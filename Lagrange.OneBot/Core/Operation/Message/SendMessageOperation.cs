bc3f02c863d6414ff75c6b69b1e47252de12c453
DarkCWK <177549718+CwkDark@users.noreply.github.com>
2024-09-14 01:16:19 +0800

    [OneBot] Fix private message recall faild

--- Lagrange.OneBot/Core/Operation/Message/SendMessageOperation.cs
+++ Lagrange.OneBot/Core/Operation/Message/SendMessageOperation.cs
@@ -24,15 +24,18 @@ public sealed class SendMessageOperation(MessageCommon common, LiteDatabase data
         };
 
         var result = await context.SendMessage(chain);
-        int hash = MessageRecord.CalcMessageHash(chain.MessageId, result.Sequence ?? 0);
+
+        if (result.Result != 0) return new OneBotResult(null, (int)result.Result, "failed");
+
+        int hash = MessageRecord.CalcMessageHash(result.MessageId, result.Sequence ?? 0);
 
         if (!chain.IsGroup) database.GetCollection<MessageRecord>().Insert(hash, new()
         {
             FriendUin = context.BotUin,
-            GroupUin = 0,
             Sequence = result.Sequence ?? 0,
-            Time = chain.Time,
-            MessageId = chain.MessageId,
+            ClientSequence = result.ClientSequence,
+            Time = DateTimeOffset.FromUnixTimeSeconds(result.Timestamp).LocalDateTime,
+            MessageId = result.MessageId,
             FriendInfo = new(
                 context.BotUin,
                 context.ContextCollection.Keystore.Uid ?? string.Empty,
@@ -41,11 +44,11 @@ public sealed class SendMessageOperation(MessageCommon common, LiteDatabase data
                 string.Empty,
                 string.Empty
             ),
-            GroupMemberInfo = null,
             Entities = chain,
-            MessageHash = hash
+            MessageHash = hash,
+            TargetUin = chain.FriendUin
         });
 
-        return new OneBotResult(new OneBotMessageResponse(hash), (int)result.Result, "ok");
+        return new OneBotResult(new OneBotMessageResponse(hash), 0, "ok");
     }
 }
