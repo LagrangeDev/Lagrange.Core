5a676cc8b539de5b691767d6f33c06f7b5063c23
pk5ls20 <pk5ls20@outlook.com>
2024-09-15 23:40:11 +0800

    [Core] Fix the issue where ForwardMsg cannot properly display (#590)
    * [Core] Fix the issue where ForwardMsg cannot properly display nickname and avatar
    
    * [Core] Fix the issue where the nickname cannot be displayed in the constructed group ForwardMsg

--- Lagrange.Core/Internal/Packets/Message/Routing/ResponseGrp.cs
+++ Lagrange.Core/Internal/Packets/Message/Routing/ResponseGrp.cs
@@ -10,5 +10,7 @@ internal class ResponseGrp
     
     [ProtoMember(4)] public string MemberName { get; set; }
     
+    [ProtoMember(5)] public uint Unknown5 { get; set; }
+    
     [ProtoMember(7)] public string GroupName { get; set; }
 }
