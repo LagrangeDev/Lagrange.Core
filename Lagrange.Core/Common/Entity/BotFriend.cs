c0074010ec8c8243821702a7ae3ebd5ed9b37b67
DarkRRb <177549718+DarkRRb@users.noreply.github.com>
2024-09-28 18:29:21 +0800

    [Core] Support custom avatar of messages in forwarding (#610)
    Co-authored-by: DarkCWK <177549718+CwkDark@users.noreply.github.com>

--- Lagrange.Core/Common/Entity/BotFriend.cs
+++ Lagrange.Core/Common/Entity/BotFriend.cs
@@ -14,9 +14,10 @@ public class BotFriend
         PersonalSign = string.Empty;
         Qid = string.Empty;
         Group = default;
+        Avatar = string.Empty;
     }
-    
-    internal BotFriend(uint uin,string uid, string nickname, string remarks, string personalSign, string qid, BotFriendGroup group = default)
+
+    internal BotFriend(uint uin, string uid, string nickname, string remarks, string personalSign, string qid, BotFriendGroup group = default)
     {
         Uin = uin;
         Uid = uid;
@@ -25,21 +26,22 @@ public class BotFriend
         PersonalSign = personalSign;
         Qid = qid;
         Group = group;
+        Avatar = $"https://q1.qlogo.cn/g?b=qq&nk={uin}&s=640";
     }
-    
+
     public uint Uin { get; set; }
-    
+
     internal string Uid { get; set; }
-    
+
     public string Nickname { get; set; }
-    
+
     public string Remarks { get; set; }
-    
+
     public string PersonalSign { get; set; }
-    
+
     public string Qid { get; set; }
-    
+
     public BotFriendGroup Group { get; set; }
 
-    public string Avatar => $"https://q1.qlogo.cn/g?b=qq&nk={Uin}&s=640";
+    public string Avatar { get; set; }
 }
