80a893da08e4f357d6c0b888c9ea2ddd3f57208e
DarkRRb <177549718+DarkRRb@users.noreply.github.com>
2024-10-01 15:52:50 +0800

    [Core] Kill `Console.WriteLine` and update internal signer implementation (#620)
    Co-authored-by: DarkRRb <177549718+CwkDark@users.noreply.github.com>

--- Lagrange.Core/Utility/Crypto/TeaImpl.cs
+++ Lagrange.Core/Utility/Crypto/TeaImpl.cs
@@ -6,14 +6,12 @@ internal class TeaImpl
 {
     public byte[] Encrypt(byte[] data, byte[] key)
     {
-        // Console.WriteLine("Data: " + data.Hex() + $" Key: {key.Hex()}");
         return TeaProvider.Encrypt(data, key);
     }
     
     public Span<byte> Decrypt(Span<byte> data, Span<byte> key)
     {
         var decipher = TeaProvider.Decrypt(data, key);
-        // Console.WriteLine("Decrypted Data: " + decipher.Hex() + $" Key: {key.Hex()}");
         return decipher;
     }
 }
