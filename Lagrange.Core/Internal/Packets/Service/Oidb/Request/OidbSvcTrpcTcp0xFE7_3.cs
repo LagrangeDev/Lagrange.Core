7de260a63e13fba03dff8684def0e795a44d72ae
脑袋里进花生了 <109732988+Redmomn@users.noreply.github.com>
2024-10-06 01:44:52 +0800

    [Core] Add Group member ShutUpTimestamp (#627)

--- Lagrange.Core/Internal/Packets/Service/Oidb/Request/OidbSvcTrpcTcp0xFE7_3.cs
+++ Lagrange.Core/Internal/Packets/Service/Oidb/Request/OidbSvcTrpcTcp0xFE7_3.cs
@@ -13,13 +13,13 @@ namespace Lagrange.Core.Internal.Packets.Service.Oidb.Request;
 internal class OidbSvcTrpcTcp0xFE7_3
 {
     [ProtoMember(1)] public uint GroupUin { get; set; }
-    
+
     [ProtoMember(2)] public uint Field2 { get; set; } // 5
-    
+
     [ProtoMember(3)] public uint Field3 { get; set; } // 2
-    
+
     [ProtoMember(4)] public OidbSvcTrpcScp0xFE7_3Body Body { get; set; }
-    
+
     [ProtoMember(15)] public string? Token { get; set; }
 }
 
@@ -27,40 +27,40 @@ internal class OidbSvcTrpcTcp0xFE7_3
 internal class OidbSvcTrpcScp0xFE7_3Body
 {
     [ProtoMember(10)] public bool MemberName { get; set; } = true; // 1
-    
+
     [ProtoMember(11)] public bool MemberCard { get; set; } = true; // 1
-    
+
     [ProtoMember(12)] public bool Level { get; set; } = true; // 1
-    
+
     [ProtoMember(13)] public bool Field13 { get; set; } = true; // 1
 
     [ProtoMember(16)] public bool Field16 { get; set; } = true; // 1
-    
+
     [ProtoMember(17)] public bool SpecialTitle { get; set; } = true; // 1
-    
+
     [ProtoMember(18)] public bool Field18 { get; set; } = true; // 1
-    
+
     [ProtoMember(20)] public bool Field20 { get; set; } = true; // 1
-    
+
     [ProtoMember(21)] public bool Field21 { get; set; } = true; // 1
-    
+
     [ProtoMember(100)] public bool JoinTimestamp { get; set; } = true; // 1
-    
+
     [ProtoMember(101)] public bool LastMsgTimestamp { get; set; } = true; // 1
-    
-    [ProtoMember(102)] public bool Field102 { get; set; } = true; // 1
-    
+
+    [ProtoMember(102)] public bool ShutUpTimestamp { get; set; } = true; // 1
+
     [ProtoMember(103)] public bool Field103 { get; set; } = true; // 1
-    
+
     [ProtoMember(104)] public bool Field104 { get; set; } = true; // 1
-    
+
     [ProtoMember(105)] public bool Field105 { get; set; } = true; // 1
-    
+
     [ProtoMember(106)] public bool Field106 { get; set; } = true; // 1
-    
+
     [ProtoMember(107)] public bool Permission { get; set; } = true; // 1
-    
+
     [ProtoMember(200)] public bool Field200 { get; set; } = true;
-    
+
     [ProtoMember(201)] public bool Field201 { get; set; } = true;
 }
