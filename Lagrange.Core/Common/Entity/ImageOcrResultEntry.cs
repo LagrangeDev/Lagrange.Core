c3631e540c26ea81a666afb2a140bd15b0db3b69
pk5ls20 <pk5ls20@outlook.com>
2024-10-28 22:55:08 +0800

    chore:

--- Lagrange.Core/Common/Entity/ImageOcrResultEntry.cs
+++ Lagrange.Core/Common/Entity/ImageOcrResultEntry.cs
@@ -5,11 +5,9 @@ namespace Lagrange.Core.Common.Entity
     [Serializable]
     public class ImageOcrResult
     {
-        [JsonPropertyName("texts")]
-        public List<TextDetection> Texts { get; set; }
+        [JsonPropertyName("texts")] public List<TextDetection> Texts { get; set; }
 
-        [JsonPropertyName("language")]
-        public string Language { get; set; }
+        [JsonPropertyName("language")] public string Language { get; set; }
 
         public ImageOcrResult(List<TextDetection> texts, string language)
         {
@@ -21,14 +19,11 @@ namespace Lagrange.Core.Common.Entity
     [Serializable]
     public class TextDetection
     {
-        [JsonPropertyName("text")]
-        public string Text { get; set; }
+        [JsonPropertyName("text")] public string Text { get; set; }
 
-        [JsonPropertyName("confidence")]
-        public int Confidence { get; set; }
+        [JsonPropertyName("confidence")] public int Confidence { get; set; }
 
-        [JsonPropertyName("coordinates")]
-        public List<Coordinate> Coordinates { get; set; }
+        [JsonPropertyName("coordinates")] public List<Coordinate> Coordinates { get; set; }
 
         public TextDetection(string text, int confidence, List<Coordinate> coordinates)
         {
@@ -41,11 +36,9 @@ namespace Lagrange.Core.Common.Entity
     [Serializable]
     public class Coordinate
     {
-        [JsonPropertyName("x")]
-        public int X { get; set; }
+        [JsonPropertyName("x")] public int X { get; set; }
 
-        [JsonPropertyName("y")]
-        public int Y { get; set; }
+        [JsonPropertyName("y")] public int Y { get; set; }
 
         public Coordinate(int x, int y)
         {
