4928500067540bd31fb8b7c0ae70e8ccd44c09c0
思思 <gamemode12345@163.com>
2024-11-04 17:03:21 +0800

    给929d 的chattype 加个默认值

--- Lagrange.Core/Internal/Packets/Service/Oidb/Request/OidbSvcTrpcTcp0x929D_0.cs
+++ Lagrange.Core/Internal/Packets/Service/Oidb/Request/OidbSvcTrpcTcp0x929D_0.cs
@@ -8,5 +8,5 @@ internal class OidbSvcTrpcTcp0x929D_0
 {
     [ProtoMember(1)] public uint Group { get; set; }
 
-    [ProtoMember(2)] public uint ChatType { get; set; }  //1 voice,2 song
+    [ProtoMember(2)] public uint ChatType { get; set; } = 1; //1 voice,2 song
 }
