b90a71446c490f2150d806e51ef113cc68fbc202
思思 <gamemode12345@163.com>
2024-11-21 18:37:24 +0800

    ob xml

--- Lagrange.Core/Message/Entity/XmlEntity.cs
+++ Lagrange.Core/Message/Entity/XmlEntity.cs
@@ -9,10 +9,20 @@ namespace Lagrange.Core.Message.Entity;
 public class XmlEntity : IMessageEntity
 {
     public string Xml { get; set; }
+
+    public int ServiceId {get;set;}
     
-    public XmlEntity() => Xml = "";
+    public XmlEntity()
+    {
+        Xml = "";
+        ServiceId = 35;
+    }
     
-    public XmlEntity(string xml) => Xml = xml;
+    public XmlEntity(string xml,int serviceid = 35)
+    {
+        Xml = xml;
+        ServiceId = serviceid;
+    }
     
     IEnumerable<Elem> IMessageEntity.PackElement()
     {
@@ -22,7 +32,7 @@ public class XmlEntity : IMessageEntity
             {
                 RichMsg = new RichMsg
                 {
-                    ServiceId = 35,
+                    ServiceId = ServiceId,
                     Template1 = ZCompression.ZCompress(Xml, new byte[] { 0x01 }),
                 }
             }
