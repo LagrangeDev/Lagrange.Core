6f0857b6a1d1a14bb75f540ec663fc48d6806460
ishkong <19740260+ishkong@users.noreply.github.com>
2024-12-04 13:05:48 +0800

    这玩意儿居然不是必须的？
    Signed-off-by: ishkong <19740260+ishkong@users.noreply.github.com>

--- Lagrange.OneBot/Core/Entity/Action/OneBotGetPrivateFile.cs
+++ Lagrange.OneBot/Core/Entity/Action/OneBotGetPrivateFile.cs
@@ -7,7 +7,7 @@ public class OneBotGetPrivateFile
 {
     [JsonPropertyName("user_id")] public uint UserId { get; set; }
 
-    [JsonPropertyName("file_hash")] public required string FileHash { get; set; }
+    [JsonPropertyName("file_hash")] public string FileHash { get; set; } = string.Empty;
     
     [JsonPropertyName("file_id")] public required string FileId { get; set; }
 }
