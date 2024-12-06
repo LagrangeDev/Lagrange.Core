967c232ead0676c49bf5ea6af6d62f18554df9fe
ishkong <19740260+ishkong@users.noreply.github.com>
2024-12-06 13:20:54 +0800

    [All] feat: implemented `get_private_file_url` (#703)
    * [All] feat: implemented `get_private_file_url`
    
    Signed-off-by: ishkong <19740260+ishkong@users.noreply.github.com>
    
    * Uniform variable name styles
    
    Signed-off-by: ishkong <19740260+ishkong@users.noreply.github.com>
    
    * 这玩意儿居然不是必须的？
    
    Signed-off-by: ishkong <19740260+ishkong@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ishkong <19740260+ishkong@users.noreply.github.com>

---
+++ Lagrange.OneBot/Core/Entity/Action/OneBotGetPrivateFile.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Lagrange.OneBot.Core.Entity.Action;
+
+[Serializable]
+public class OneBotGetPrivateFile
+{
+    [JsonPropertyName("user_id")] public uint UserId { get; set; }
+
+    [JsonPropertyName("file_hash")] public string FileHash { get; set; } = string.Empty;
+    
+    [JsonPropertyName("file_id")] public required string FileId { get; set; }
+}
