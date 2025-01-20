20b656e0755b51a0a3e524dd1cbee64ec5bb0fc2
RikaCelery <94585272+RikaCelery@users.noreply.github.com>
2025-01-20 14:38:54 +0800

    [OneBot] get_rkey (#742)

--- Lagrange.Core/Internal/Event/System/FetchRKeyEvent.cs
+++ Lagrange.Core/Internal/Event/System/FetchRKeyEvent.cs
@@ -1,19 +1,19 @@
+using Lagrange.Core.Internal.Packets.Service.Oidb.Common;
+
 namespace Lagrange.Core.Internal.Event.System;
 
 internal class FetchRKeyEvent : ProtocolEvent
 {
-    public List<string> RKeys { get; } = new();
+    public List<RKeyInfo> RKeys { get; } = new();
 
-    private FetchRKeyEvent() : base(true)
-    {
-    }
+    private FetchRKeyEvent() : base(true) { }
 
-    private FetchRKeyEvent(int resultCode, List<string> rKeys) : base(resultCode)
+    private FetchRKeyEvent(int resultCode, List<RKeyInfo> rKeys) : base(resultCode)
     {
         RKeys = rKeys;
     }
-    
+
     public static FetchRKeyEvent Create() => new();
-    
-    public static FetchRKeyEvent Result(int resultCode, List<string> rKeys) => new(resultCode, rKeys);
+
+    public static FetchRKeyEvent Result(int resultCode, List<RKeyInfo> rKeys) => new(resultCode, rKeys);
 }
