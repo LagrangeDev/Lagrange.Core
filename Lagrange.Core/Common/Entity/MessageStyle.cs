1ec3105b5b55ba1b7fab625ae746d44ef73c1097
dogdie233 <dogdie233@163.com>
2025-02-03 15:08:59 +0800

    [Core] Add message style

---
+++ Lagrange.Core/Common/Entity/MessageStyle.cs
@@ -0,0 +1,34 @@
+﻿namespace Lagrange.Core.Common.Entity;
+
+public class MessageStyle
+{
+    /// <summary>
+    /// 气泡id
+    /// </summary>
+    public uint BubbleId { get; set; }
+
+    /// <summary>
+    /// 挂件id
+    /// </summary>
+    public long PendantId { get; set; }
+
+    /// <summary>
+    /// 字体id
+    /// </summary>
+    public ushort FontId { get; set; }
+
+    /// <summary>
+    /// 字体特效id，超大字为2000
+    /// </summary>
+    public uint FontEffectId { get; set; }
+
+    /// <summary>
+    /// 是否启用大小字特效
+    /// </summary>
+    public bool IsCsFontEffectEnabled { get; set; }
+
+    /// <summary>
+    /// 自定义气泡数据存档id
+    /// </summary>
+    public uint BubbleDiyTextId { get; set; }
+}
---
+++ Lagrange.Core/Internal/Packets/Message/Component/Extra/MessageStyleExtra.cs
@@ -0,0 +1,34 @@
+﻿using ProtoBuf;
+
+namespace Lagrange.Core.Internal.Packets.Message.Component.Extra;
+
+[ProtoContract]
+internal class MessageStyleExtra
+{
+    [ProtoMember(15)] public ulong Font { get; set; }
+    [ProtoMember(34)] public uint FontEffectId { get; set; }
+
+    [ProtoMember(51)] public ulong VipType { get; set; } = 337;
+
+    [ProtoMember(52)] public ulong VipLevel { get; set; } = 2;
+
+    [ProtoMember(56)] public ulong VipNameplate { get; set; }
+
+    [ProtoMember(65)] public GroupMemberLevelInfo? GroupMemberLevel { get; set; }
+
+    [ProtoMember(66)] public int Field66 { get; set; } = 128;
+
+    [ProtoMember(71)] public int Field71 { get; set; } = 2; // 258;
+
+    [ProtoMember(79)] public uint Field79 { get; set; } = 131080;
+
+    [ProtoMember(81)] public ulong GroupHonorStyle { get; set; }
+
+    [ProtoMember(107)] public uint MessageSequence { get; set; }
+
+    [ProtoContract]
+    internal class GroupMemberLevelInfo
+    {
+        [ProtoMember(2)] public int Level { get; set; }
+    }
+}
