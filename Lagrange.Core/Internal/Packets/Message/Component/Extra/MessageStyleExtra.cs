396d7dd851cda0a3fdd949615a97411e0ecaf6cd
dogdie233 <dogdie233@163.com>
2025-02-03 15:12:28 +0800

    add and remove some fields in MessageStyleExtra.cs

--- Lagrange.Core/Internal/Packets/Message/Component/Extra/MessageStyleExtra.cs
+++ Lagrange.Core/Internal/Packets/Message/Component/Extra/MessageStyleExtra.cs
@@ -5,6 +5,10 @@ namespace Lagrange.Core.Internal.Packets.Message.Component.Extra;
 [ProtoContract]
 internal class MessageStyleExtra
 {
+    /// <summary>
+    /// 如果是管理员/群主则会有这个field并且值是10315
+    /// </summary>
+    [ProtoMember(4)] public int Field4 { get; set; } = 10315;
     [ProtoMember(15)] public ulong Font { get; set; }
     [ProtoMember(34)] public uint FontEffectId { get; set; }
 
@@ -15,12 +19,6 @@ internal class MessageStyleExtra
     [ProtoMember(56)] public ulong VipNameplate { get; set; }
 
     [ProtoMember(65)] public GroupMemberLevelInfo? GroupMemberLevel { get; set; }
-
-    [ProtoMember(66)] public int Field66 { get; set; } = 128;
-
-    [ProtoMember(71)] public int Field71 { get; set; } = 2; // 258;
-
-    [ProtoMember(79)] public uint Field79 { get; set; } = 131080;
 
     [ProtoMember(81)] public ulong GroupHonorStyle { get; set; }
 
