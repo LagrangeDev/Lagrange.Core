cdbae32282c86bc831c7b913efa77d629b083200
dogdie233 <dogdie233@163.com>
2025-02-09 17:31:17 +0800

    ababa

--- Lagrange.OneBot/Core/Entity/OnebotMessageStyle.cs
+++ Lagrange.OneBot/Core/Entity/OnebotMessageStyle.cs
@@ -3,17 +3,31 @@ using Lagrange.Core.Common.Entity;
 
 namespace Lagrange.OneBot.Core.Entity;
 
-public class OnebotMessageStyle(MessageStyle style)
+public class OnebotMessageStyle
 {
-    [JsonPropertyName("bubble_id")] public ulong BubbleId { get; set; } = style.BubbleId;
+    [JsonPropertyName("bubble_id")] public ulong BubbleId { get; set; }
 
-    [JsonPropertyName("pendant_id")] public ulong PendantId { get; set; } = style.PendantId;
+    [JsonPropertyName("pendant_id")] public ulong PendantId { get; set; }
 
-    [JsonPropertyName("font_id")] public ushort FontId { get; set; } = style.FontId;
+    [JsonPropertyName("font_id")] public ushort FontId { get; set; }
 
-    [JsonPropertyName("font_effect_id")] public uint FontEffectId { get; set; } = style.FontEffectId;
+    [JsonPropertyName("font_effect_id")] public uint FontEffectId { get; set; }
 
-    [JsonPropertyName("is_cs_font_effect_enabled")] public bool IsCsFontEffectEnabled { get; set; } = style.IsCsFontEffectEnabled;
+    [JsonPropertyName("is_cs_font_effect_enabled")] public bool IsCsFontEffectEnabled { get; set; }
 
-    [JsonPropertyName("bubble_diy_text_id")] public uint BubbleDiyTextId { get; set; } = style.BubbleDiyTextId;
+    [JsonPropertyName("bubble_diy_text_id")] public uint BubbleDiyTextId { get; set; }
+
+    public OnebotMessageStyle()
+    {
+    }
+
+    public OnebotMessageStyle(OnebotMessageStyle style)
+    {
+        BubbleId = style.BubbleId;
+        PendantId = style.PendantId;
+        FontId = style.FontId;
+        FontEffectId = style.FontEffectId;
+        IsCsFontEffectEnabled = style.IsCsFontEffectEnabled;
+        BubbleDiyTextId = style.BubbleDiyTextId;
+    }
 }
