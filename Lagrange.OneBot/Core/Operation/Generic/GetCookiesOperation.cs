efb4b3927a350a22afb7261184a3e8265017ebd4
pk5ls20 <pk5ls20@outlook.com>
2025-02-14 20:21:16 +0800

    feat: get cookies return `ticket.GetCookies`

--- Lagrange.OneBot/Core/Operation/Generic/GetCookiesOperation.cs
+++ Lagrange.OneBot/Core/Operation/Generic/GetCookiesOperation.cs
@@ -1,19 +1,19 @@
 using System.Text.Json.Nodes;
 using Lagrange.Core;
-using Lagrange.Core.Common.Interface.Api;
 using Lagrange.OneBot.Core.Entity.Action;
+using Lagrange.OneBot.Core.Notify;
 
 namespace Lagrange.OneBot.Core.Operation.Generic;
 
 [Operation("get_cookies")]
-public class GetCookiesOperation : IOperation
+public class GetCookiesOperation (TicketService ticket) : IOperation
 {
     public async Task<OneBotResult> HandleOperation(BotContext context, JsonNode? payload)
     {
         if (payload?["domain"]?.ToString() is { } domain)
         {
-            var cookies = await context.FetchCookies([domain]);
-            return new OneBotResult(new JsonObject { { "cookies", cookies[0] } }, 0, "ok");
+            var cookies = await ticket.GetCookies(domain);
+            return new OneBotResult(new JsonObject { { "cookies", cookies } }, 0, "ok");
         }
         
         throw new Exception();
