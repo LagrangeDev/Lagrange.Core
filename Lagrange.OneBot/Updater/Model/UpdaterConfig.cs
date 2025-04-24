0165c386c6d5cb4f5e3b7a53f0f8074c1057a39b
Lilyltt <1806552019@qq.com>
2025-04-24 20:38:00 +0800

    [OneBot] make default enable auto update false

--- Lagrange.OneBot/Updater/Model/UpdaterConfig.cs
+++ Lagrange.OneBot/Updater/Model/UpdaterConfig.cs
@@ -2,7 +2,7 @@ namespace Lagrange.OneBot.Updater.Model
 {
     public class UpdaterConfig
     {
-        public bool EnableAutoUpdate { get; set; } = true;
+        public bool EnableAutoUpdate { get; set; } = false;
         public int CheckInterval { get; set; } = 360;
         public string ProxyUrl { get; set; } = string.Empty;
         public string LastUpdateTime { get; set; } = string.Empty;
