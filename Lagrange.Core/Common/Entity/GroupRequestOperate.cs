e6415e8ba0e3e75ac372f504fd8f741155009236
DarkRRb <177549718+DarkRRb@users.noreply.github.com>
2025-05-02 01:11:04 +0800

    [Core] add ignore group request (#819)

---
+++ Lagrange.Core/Common/Entity/GroupRequestOperate.cs
@@ -0,0 +1,8 @@
+namespace Lagrange.Core.Common.Entity;
+
+public enum GroupRequestOperate
+{
+    Allow = 1,
+    Deny = 2,
+    Ignore = 3,
+}
--- Lagrange.OneBot/Core/Operation/Request/SetGroupAddRequestOperation.cs
+++ Lagrange.OneBot/Core/Operation/Request/SetGroupAddRequestOperation.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Lagrange.Core;
+using Lagrange.Core.Common.Entity;
 using Lagrange.OneBot.Core.Entity.Action;
 using Lagrange.OneBot.Core.Operation.Converters;
 
@@ -18,13 +19,13 @@ public class SetGroupAddRequestOperation : IOperation
             uint groupUin = uint.Parse(split[1]);
             uint eventType = uint.Parse(split[2]);
             bool isFiltered = Convert.ToBoolean(uint.Parse(split.Length > 3 ? split[3] : "0"));
-            
+            var operate = request.Approve ? GroupRequestOperate.Allow : GroupRequestOperate.Deny;
+
+            var logic = context.ContextCollection.Business.OperationLogic;
             bool result = isFiltered
-                ? await context.ContextCollection.Business.OperationLogic.SetGroupFilteredRequest(groupUin, sequence,
-                    eventType, request.Approve, request.Reason)
-                : await context.ContextCollection.Business.OperationLogic.SetGroupRequest(groupUin, sequence, eventType,
-                    request.Approve, request.Reason);
-            return new OneBotResult(null, result ? 0 : 1, "ok");
+                ? await logic.SetGroupFilteredRequest(groupUin, sequence, eventType, operate, request.Reason)
+                : await logic.SetGroupRequest(groupUin, sequence, eventType, operate, request.Reason);
+            return new OneBotResult(null, result ? 0 : 1, result ? "ok" : "failed");
         }
 
         throw new Exception();
