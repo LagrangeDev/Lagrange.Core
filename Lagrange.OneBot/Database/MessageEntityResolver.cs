225e1f0bef39037c70d4b5caa7e1c1ac3a3b583c
DarkRRb <177549718+DarkRRb@users.noreply.github.com>
2025-05-07 01:44:10 +0800

    [OneBot] Fix ForwardEntity cannot be serialized (#824)

--- Lagrange.OneBot/Database/MessageEntityResolver.cs
+++ Lagrange.OneBot/Database/MessageEntityResolver.cs
@@ -6,11 +6,15 @@ namespace Lagrange.OneBot.Database;
 
 public class MessageEntityResolver : IFormatterResolver
 {
-    private static readonly MessageEntityFormatter _FORMATTER = new();
+    private static readonly MessageEntityFormatter ENTITY_FORMATTER = new();
+
+    private static readonly MessageChainFormatter CHAIN_FORMATTER = new();
 
     public IMessagePackFormatter<T>? GetFormatter<T>()
     {
-        if (typeof(T) == typeof(IMessageEntity)) return (IMessagePackFormatter<T>?)_FORMATTER;
+        if (typeof(T) == typeof(IMessageEntity)) return (IMessagePackFormatter<T>)ENTITY_FORMATTER;
+
+        if (typeof(T) == typeof(MessageChain)) return (IMessagePackFormatter<T>)CHAIN_FORMATTER;
 
         return null;
     }
