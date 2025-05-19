297389faf2c23449b5026919048b085f9cec8da1
手瓜一十雪 <nanaeonn@outlook.com>
2025-05-19 13:09:14 +0800

    [Core] [OneBot] feat: poke target_id (#836)
    * feat: poke target_id
    
    * fix: 删除多余定义

--- Lagrange.OneBot/Core/Operation/Message/FriendPokeOperation.cs
+++ Lagrange.OneBot/Core/Operation/Message/FriendPokeOperation.cs
@@ -1,23 +1,4 @@
-using System.Text.Json;
-using System.Text.Json.Nodes;
-using Lagrange.Core;
-using Lagrange.Core.Common.Interface.Api;
-using Lagrange.OneBot.Core.Entity.Action;
-using Lagrange.OneBot.Core.Operation.Converters;
-
 namespace Lagrange.OneBot.Core.Operation.Message;
 
 [Operation("friend_poke")]
-public class FriendPokeOperation : IOperation
-{
-    public async Task<OneBotResult> HandleOperation(BotContext context, JsonNode? payload)
-    {
-        if (payload.Deserialize<OneBotFriendPoke>(SerializerOptions.DefaultOptions) is { } poke)
-        {
-            bool result = await context.FriendPoke(poke.UserId);
-            return new OneBotResult(null, result ? 0 : 1, result ? "ok" : "failed");
-        }
-
-        throw new Exception();
-    }
-}
+public class FriendPokeOperation : SendPokeOperation { }
