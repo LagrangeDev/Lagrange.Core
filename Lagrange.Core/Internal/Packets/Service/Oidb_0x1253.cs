a238b781a6167d8b58e9e915dd9430ae0f83ff50
majon <majonokakera@163.com>
2026-07-28 22:52:30 +0800

    [Core] Add group member mute support

---
+++ Lagrange.Core/Internal/Packets/Service/Oidb_0x1253.cs
@@ -0,0 +1,29 @@
+using Lagrange.Proto;
+
+namespace Lagrange.Core.Internal.Packets.Service;
+
+#pragma warning disable CS8618
+
+[ProtoPackable]
+internal partial class D1253ReqBody
+{
+    [ProtoPackable]
+    internal partial class MuteInfo
+    {
+        [ProtoMember(1)] public string TargetUid { get; set; }
+
+        [ProtoMember(2)] public uint Duration { get; set; }
+    }
+
+    [ProtoMember(1)] public uint GroupUin { get; set; }
+
+    [ProtoMember(2)] public uint Type { get; set; }
+
+    [ProtoMember(3)] public MuteInfo Body { get; set; }
+}
+
+[ProtoPackable]
+internal partial class D1253RspBody
+{
+    [ProtoMember(2)] public string? Success { get; set; }
+}
